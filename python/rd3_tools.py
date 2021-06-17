8d7e11b40240680c3ea3288e6f6eb2123a4e9f54
davidruvolo51 <davidruvolo51@gmail.com>
2021-06-17 15:33:42 +0200

    added methods for reading metadata from the cluster

--- python/rd3_tools.py
+++ python/rd3_tools.py
@@ -9,25 +9,28 @@
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
-# define imports
+# declare imports
 import molgenis.client as molgenis
-from urllib.parse import quote_plus
-from datetime import datetime
-import requests
+import subprocess
 import mimetypes
+import requests
 import json
 import yaml
 import os
 
+from urllib.parse import quote_plus
+from datetime import datetime
+
 # @title molgenis
 # @description extend molgenis class
 class molgenis(molgenis.Session):
-    # @title Upodate Table
+    # @title Update Table
     # @name update_table
     # @description batch update a molgenis entity
     # @param self required class param
     # @param data object containing data to import
     # @param entity ID of the target entity written as 'package_entity'
+    # @return a response code
     def update_table(self, data, entity):
         for d in range(0, len(data), 1000):
             response = self._session.post(
@@ -49,6 +52,7 @@ class molgenis(molgenis.Session):
     # @param values data to import, a list of dictionaries where each dictionary
     #       is structured with two keys: the ID attribute and the attribute
     #       that you wish to update. E.g. [{'id': 'id123", 'x': 1},...]
+    # @return a response code
     def batch_update_one_attr(self, entity, attr, values):
         add = 'No new data'
         for i in range(0, len(values), 1000):
@@ -74,6 +78,7 @@ class molgenis(molgenis.Session):
     # @param self required molgenis param
     # @param name name of the file to use in Molgenis
     # @param path location to the file
+    # @return a response code
     def upload_file(self, name, path):
         filepath = os.path.abspath(path)
         url = self._url + 'files/'
@@ -97,6 +102,70 @@ class molgenis(molgenis.Session):
             )
         else:
             response.raise_for_status()
+
+# @title cluster_list_files
+# @description return a list of dictionaries containing available files
+# @param path location to directory
+# @return a list of dictionaries
+def cluster_list_files(path):
+    available_files = subprocess.Popen(
+        ['ssh', 'corridor+fender', 'ls', path],
+        stdout = subprocess.PIPE,
+        stdin = subprocess.PIPE,
+        stderr= subprocess.PIPE,
+        universal_newlines=True
+    )
+    data = []
+    for f in available_files.stdout:
+        data.append({
+            'file_name': f.strip(),
+            'file_path': path + f.strip()
+        })
+    available_files.kill()
+    print('Found', len(data), 'files')
+    return data
+
+# @title cluster_read_file
+# @description read contents of a file on the cluster; ideally for ped files
+# @param path location of a file; use output from cluster_list_files
+# @return list object
+def cluster_read_file(path):
+    proc = subprocess.Popen(
+        ['ssh', 'corridor+fender', 'cat', path],
+        stdout = subprocess.PIPE,
+        stdin = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        universal_newlines=True
+    )
+    proc.wait()
+    data= []
+    for line in proc.stdout:
+        data.append(line.strip())
+    proc.kill()
+    return data
+
+# @title cluster_read_json
+# @description read contents of json file
+# @param path location of the file; use output from cluster_list_files
+# @return a list object
+def cluster_read_json(path):
+    proc = subprocess.Popen(
+        ['ssh', 'corridor+fender', 'cat', path],
+        stdout = subprocess.PIPE,
+        stdin = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        universal_newlines=True
+    )
+    proc.wait()
+    try:
+        raw = proc.communicate(timeout=15)
+        data = json.loads(raw[0])
+        proc.kill()
+        return data
+    except subprocess.TimeoutExpired:
+        print('Error: unable to fetch file ' + str(os.path.basename(path)))
+        proc.kill()
+        return ''
 
 # @title distinct_dict
 # @description get distinct dictionnaires only
@@ -167,6 +236,14 @@ def load_yaml_config(path):
         f.close()
     return d
 
+# @title load_json
+# @description read json file
+# @param path location to the file
+def load_json(path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+        file.close()
+    return data
 
 # @title Get missing value count by key
 # @description get number of missing values by key

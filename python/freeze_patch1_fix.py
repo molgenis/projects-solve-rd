baeeb8f948f69abb235c83a5e167c2123f03a081
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-04 16:04:51 +0200

    add initial PED file import

--- python/freeze_patch1_fix.py
+++ python/freeze_patch1_fix.py
@@ -2,7 +2,7 @@
 #' FILE: freeze_patch1_fix.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-06-30
-#' MODIFIED: 2021-07-14
+#' MODIFIED: 2021-08-04
 #' PURPOSE: pull patch1 data and updates cases
 #' STATUS: in.progress
 #' PACKAGES: rd3tools
@@ -39,6 +39,7 @@ def process__filematches(data, ref, label = 'status'):
         else:
             d[label] = 'no'
 
+#'/////////////////////////////////////
 
 # compile a list of files per release
 pheno_files_f1 = rd3tools.cluster_list_files(
@@ -69,7 +70,7 @@ sum(d.get('inFreeze1') == 'yes' for d in pheno_files_f1p1)
 # using phenopacket files, look for changes in the files
 starttime = datetime.utcnow().strftime('%H:%M:%S.%f')[:-4]
 patch_1_ids = []
-for p in pheno_files_f1p1[7550:]:
+for p in pheno_files_f1p1:
     rd3tools.status_msg('Processing data for {}...'.format(p['subjectID']))
     if p['inFreeze1']:
         # pull matching dictionary in Freeze1 original dataset
@@ -153,6 +154,18 @@ for p in pheno_files_f1p1[7550:]:
                     likely_p1 += 1
             else:
                 rd3tools.status_msg('No diagnostic data detected')
+            # check onset codes
+            if 'ageOfOnset' in p1:
+                rd3tools.status_msg('Evaluating onset code...')
+                if 'ageOfOnset' in f1:
+                    if p1['ageOfOnset'] != f1['ageOfOnset']:
+                        rd3tools.status_msg('Onset code is new (likely P1)')
+                        likely_p1 += 1
+                else:
+                    rd3tools.status_msg('Onset code is new (likely P1)')
+                    likely_p1 += 1
+            else:
+                rd3tools.status_msg('No onset code detected')
             # process counter
             if likely_p1 > -1:
                 rd3tools.status_msg('ID {} is likely Patch1'.format(p1['id']))
@@ -189,8 +202,8 @@ for d in patch_1_ids:
 
 # start molgenis session
 rd3 = rd3tools.molgenis(
-    url = config['hosts']['acc'],
-    token = config['tokens']['acc']
+    url = config['hosts']['prod'],
+    token = config['tokens']['prod']
 )
 
 # push
@@ -209,21 +222,21 @@ rd3.batch_update_one_attr(
 #//////////////////////////////////////
 
 # pull metadata from RD3
-freeze_subjects = rd3.get(
-    entity = config['releases']['freeze1-patch1']['subject'],
-    attributes='id,subjectID,sex1'
-)
-subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
+# freeze_subjects = rd3.get(
+#     entity = config['releases']['freeze1-patch1']['subject'],
+#     attributes='id,subjectID,sex1'
+# )
+# subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
 
 # pull file metadata
-rd3_phenopacket_metadata = rd3.get(
-    entity = config['releases']['freeze1-patch1']['file'],
-    attributes = 'EGA,name,md5,patch,typeFile,dateCreated',
-    q = 'typeFile==json'
-)
+# rd3_phenopacket_metadata = rd3.get(
+#     entity = config['releases']['freeze1-patch1']['file'],
+#     attributes = 'EGA,name,md5,patch,typeFile,dateCreated',
+#     q = 'typeFile==json'
+# )
 
-rd3tools.flatten_attr(rd3_phenopacket_metadata, 'dateCreated', distinct=True)
-rd3tools.find_dict(rd3_phenopacket_metadata, 'dateCreated', '2021-05-14')[:10]
+# rd3tools.flatten_attr(rd3_phenopacket_metadata, 'dateCreated', distinct=True)
+# rd3tools.find_dict(rd3_phenopacket_metadata, 'dateCreated', '2021-05-14')[:10]
 
 # Create filename column
 # for freeze_file in rd3_phenopacket_metadata:
@@ -231,47 +244,97 @@ rd3tools.find_dict(rd3_phenopacket_metadata, 'dateCreated', '2021-05-14')[:10]
 #     freeze_file['filename'] = re.split(r'(\.[0-9]{11,}\.cip)', freeze_file['filename'])[0]
 
 
-#//////////////////////////////////////
+#'////////////////////////////////////////////////////////////////////////////
 
 # ~ 1 ~
-# Pull all available PED files stored on the cluster
-# and process
+# Pull all available PED files stored on the cluster and process
 
-# pedfiles_raw = rd3tools.cluster_list_files(
-#     path = config['releases']['base'] + config['releases']['freeze1-patch1']['ped']
-# )
-
-# remove uncessary files
-# pedfiles = []
-# for ped_file in pedfiles_raw:
-#     if re.search(r'(\.ped|\.ped.cip)$', ped_file.get('file_name')):
-#         pedfiles.append(ped_file)
-# del ped_file
-
-# check - should be 4871
-# len(pedfiles)
-
-# extract contents for PED files (this will take some time)
-# raw_pedigree_data = []
-# for file in pedfiles[:25]:
-#     rd3tools.status_msg('Processing file {}'.format(file['file_name']))
-#     contents = rd3tools.cluster_read_file(path = file['file_path'])
-#     data = rd3tools.ped_extract_contents(
-#         contents = contents,
-#         ids = subject_ids,
-#         filename = file['file_name']
-#     )[0]
-#     raw_pedigree_data.append(data)
+freeze_subjects = rd3.get(
+    entity = config['releases']['freeze1']['subject'],
+    attributes='id,subjectID,sex1'
+)
+subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
 
 
-# # clean up
-# del file, contents, data
+# @name ped__extract__id
+# @description extract FamilyID from filename
+# @param data dataset to process (output from `cluster_list_files`)
+# @return a list of dictionaries
+def ped__extract__id(data):
+    for d in data:
+        d['familyID'] = re.sub('((.ped)|(.ped.cip))$', '', d['filename'])
 
-# pedigree_data = []
-# for ped in raw_pedigree_data:
-#     if ped['upload']:
-#         pedigree_data.append(ped)
+
+#'/////////////////////////////////////
+
+ped_files_f1 = rd3tools.cluster_list_files(
+    path = config['releases']['base'] + config['releases']['freeze1']['ped'],
+    filter = '((.ped)|(.ped.cip))$'
+)
+
+ped_files_p1 = rd3tools.cluster_list_files(
+    path = config['releases']['base'] + config['releases']['freeze1-patch1']['ped'],
+    filter = '((.ped)|(.ped.cip))$'
+)
 
 
-# #//////////////////////////////////////
+# extract family ID from filename
+ped__extract__id(data = ped_files_f1)
+ped__extract__id(data = ped_files_p1)
+
+
+# extract metadata
+ped_f1 = []
+ped_p1 = []
+
+
+# freeze1 files
+for file in ped_files_f1:
+    rd3tools.status_msg('Processing file:', file['filename'])
+    raw = rd3tools.cluster_read_file(file['filepath'])
+    data = rd3tools.ped_extract_contents(
+        contents = raw,
+        ids = subject_ids,
+        filename = file['filename']
+    )
+    if len(data) > 1:
+        for line in data:
+            if line['upload']:
+                ped_f1.append(line)
+    else:
+        if data[0]['upload']:
+            ped_f1.append(data[0])
+
+del file, raw, data, line
+
+# patch1 files
+for file in ped_files_p1:
+    rd3tools.status_msg('Processing file:', file['filename'])
+    raw = rd3tools.cluster_read_file(file['filepath'])
+    data = rd3tools.ped_extract_contents(
+        contents = raw,
+        ids = subject_ids,
+        filename = file['filename']
+    )
+    if len(data) > 1:
+        for line in data:
+            if line['upload']:
+                ped_p1.append(line)
+    else:
+        if data[0]['upload']:
+            ped_p1.append(data[0])
+
+del file, raw, data, line
+
+
+# compare files
+for p in ped_p1[:1]:
+    r = rd3tools.find_dict(ped_f1, 'id', p['id'])
+    if r:
+        f = r[0]
+        
+
+
+
+
 

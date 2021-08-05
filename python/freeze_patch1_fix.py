ec9d71ce1233321c0c7637cf560015a028866ce7
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-05 16:52:01 +0200

    added checks for f1 and p1 comparisons

--- python/freeze_patch1_fix.py
+++ python/freeze_patch1_fix.py
@@ -18,7 +18,7 @@ config = rd3tools.load_yaml_config('python/_config.yml')
 # @description extract subjectID from filepath
 # @param data list of dictionaries containing filenames and path
 # @return a list of dictionaries with filename, filepath, subjectID
-def process__filename(data):
+def pheno__process__filename(data):
     for d in data:
         match = re.search(r'^(P[0-9]{7})', d.get('filename'))
         if match:
@@ -26,12 +26,13 @@ def process__filename(data):
         else:
             d['subjectID'] = None
 
+
 # @name process__filematches
 # @description determine if an ID from one object exists in another object
 # @param data the main dataset containg IDs you want to use to search another object
 # @param ref object to search through
 # @param label name of the attribute to create
-def process__filematches(data, ref, label = 'status'):
+def pheno__process__filematches(data, ref, label = 'status'):
     ids = rd3tools.flatten_attr(data = ref, attr = 'subjectID')
     for d in data:
         if d.get('subjectID') in ids:
@@ -39,7 +40,39 @@ def process__filematches(data, ref, label = 'status'):
         else:
             d[label] = 'no'
 
-#'/////////////////////////////////////
+
+# @name ped__extract__id
+# @description extract FamilyID from filename
+# @param data dataset to process (output from `cluster_list_files`)
+# @return a list of dictionaries
+def ped__extract__id(data):
+    for d in data:
+        d['familyID'] = re.sub('((.ped)|(.ped.cip))$', '', d['filename'])
+
+
+#'/////////////////////////////////////////////////////////////////////////////
+
+# ~ 1 ~
+# Init Molgenis and grab metadata
+
+# start molgenis session
+rd3 = rd3tools.molgenis(
+    url = config['hosts']['prod'],
+    token = config['tokens']['prod']
+)
+
+freeze_subjects = rd3.get(
+    entity = config['releases']['freeze1']['subject'],
+    attributes='id,subjectID,sex1'
+)
+
+subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
+
+
+#'/////////////////////////////////////////////////////////////////////////////
+
+# ~ 2 ~
+# Phenopackets Check
 
 # compile a list of files per release
 pheno_files_f1 = rd3tools.cluster_list_files(
@@ -53,12 +86,21 @@ pheno_files_f1p1 = rd3tools.cluster_list_files(
 )
 
 # extract subject ID from filename
-process__filename(data = pheno_files_f1)
-process__filename(data = pheno_files_f1p1)
+pheno__process__filename(data = pheno_files_f1)
+pheno__process__filename(data = pheno_files_f1p1)
 
 # check to see if a file exists in the other freeze
-process__filematches(data = pheno_files_f1, ref = pheno_files_f1p1, label = 'inPatch1')
-process__filematches(data = pheno_files_f1p1, ref = pheno_files_f1, label = 'inFreeze1')
+pheno__process__filematches(
+    data = pheno_files_f1,
+    ref = pheno_files_f1p1,
+    label = 'inPatch1'
+)
+
+pheno__process__filematches(
+    data = pheno_files_f1p1,
+    ref = pheno_files_f1,
+    label = 'inFreeze1'
+)
 
 # get counts
 len(pheno_files_f1)
@@ -189,9 +231,6 @@ rd3tools.status_msg(
     )
 )
 
-
-#//////////////////////////////////////
-
 # update data in RD3
 patch_1_updates = []
 for d in patch_1_ids:
@@ -199,12 +238,6 @@ for d in patch_1_ids:
         'id': d.get('id') + '_original',
         'patch': 'freeze1_original,freeze1_patch1'
     })
-
-# start molgenis session
-rd3 = rd3tools.molgenis(
-    url = config['hosts']['prod'],
-    token = config['tokens']['prod']
-)
 
 # push
 rd3.batch_update_one_attr(
@@ -219,53 +252,11 @@ rd3.batch_update_one_attr(
     values = patch_1_updates
 )
 
-#//////////////////////////////////////
-
-# pull metadata from RD3
-# freeze_subjects = rd3.get(
-#     entity = config['releases']['freeze1-patch1']['subject'],
-#     attributes='id,subjectID,sex1'
-# )
-# subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
-
-# pull file metadata
-# rd3_phenopacket_metadata = rd3.get(
-#     entity = config['releases']['freeze1-patch1']['file'],
-#     attributes = 'EGA,name,md5,patch,typeFile,dateCreated',
-#     q = 'typeFile==json'
-# )
-
-# rd3tools.flatten_attr(rd3_phenopacket_metadata, 'dateCreated', distinct=True)
-# rd3tools.find_dict(rd3_phenopacket_metadata, 'dateCreated', '2021-05-14')[:10]
-
-# Create filename column
-# for freeze_file in rd3_phenopacket_metadata:
-#     freeze_file['filename'] = os.path.basename(freeze_file['name'])
-#     freeze_file['filename'] = re.split(r'(\.[0-9]{11,}\.cip)', freeze_file['filename'])[0]
-
-
 #'////////////////////////////////////////////////////////////////////////////
 
-# ~ 1 ~
+# ~ 2 ~
+# PED file checks
 # Pull all available PED files stored on the cluster and process
-
-freeze_subjects = rd3.get(
-    entity = config['releases']['freeze1']['subject'],
-    attributes='id,subjectID,sex1'
-)
-subject_ids = rd3tools.flatten_attr(data = freeze_subjects, attr = 'subjectID')
-
-
-# @name ped__extract__id
-# @description extract FamilyID from filename
-# @param data dataset to process (output from `cluster_list_files`)
-# @return a list of dictionaries
-def ped__extract__id(data):
-    for d in data:
-        d['familyID'] = re.sub('((.ped)|(.ped.cip))$', '', d['filename'])
-
-
-#'/////////////////////////////////////
 
 ped_files_f1 = rd3tools.cluster_list_files(
     path = config['releases']['base'] + config['releases']['freeze1']['ped'],
@@ -328,25 +319,166 @@ del file, raw, data, line
 
 
 # compare files
-for p in ped_p1[:1]:
-    p['isPatch1'] = False
+for p in ped_p1:
+    rd3tools.status_msg('Processing', p['id'])
+    p['isPatch1'] = 'no'
+    p['new_mid'] = 'no'
+    p['new_pid'] = 'no'
+    p['new_file'] = 'no'
+    p['new_clinical_status'] = 'no'
+    p['new_sex1'] = 'no'
     r = rd3tools.find_dict(ped_f1, 'id', p['id'])
     if r:
         f = r[0]
-        if str(p['mid'] != 'None'):
-            if str(f['mid'] == 'None'):
-                p['isPatch1'] = True
-        if str(p['pid'] != 'None'):
-            if str(f['pid'] == 'None'):
-                p['isPatch1'] = True
-        if str(p['sex1'] != 'None'):
-            if str(f['sex1'] == 'None'):
-                p['isPatch1'] = True
-        if str(p['clinical_status'] != 'None'):
-            if str(f['clinical_status'] == 'None'):
-                p['isPatch1'] = True
-            else:
-                if f['clinical_status'] != p['clinical_status']:
-                    p['isPatch1'] = True
+        if p['mid'] is not None and f['mid'] is None:
+            rd3tools.status_msg('New maternal ID detected')
+            p['isPatch1'] = 'yes'
+            p['new_mid'] = 'yes'
+        if p['pid'] is not None and f['pid'] is None:
+            rd3tools.status_msg('New paternal ID detected')
+            p['isPatch1'] = 'yes'
+            p['new_pid'] = 'yes'
+        if p['sex1'] is not None and f['sex1'] is None:
+            rd3tools.status_msg('New sex code detected')
+            p['isPatch1'] = 'yes'
+            p['new_sex'] = 'yes'
+        if p['clinical_status'] is not None:
+            if f['clinical_status'] is None:
+                rd3tools.status_msg('New clinical status detected')
+                p['isPatch1'] = 'yes'
+                p['new_clinical_status'] = 'yes'
+            elif p['clinical_status'] != f['clinical_status']:
+                rd3tools.status_msg('New clinical status detected')
+                p['isPatch1'] = 'yes'
+                p['new_cinical_status'] = 'yes'
+    else:
+        rd3tools.status_msg('New file detected')
+        if p['mid'] is not None:
+            p['isPatch1'] = 'yes'
+            p['new_mid'] = 'yes'
+            p['new_file'] = 'yes'
+        if p['pid'] is not None:
+            p['isPatch1'] = 'yes'
+            p['new_pid'] = 'yes'
+            p['new_file'] = 'yes'
+        if p['sex1'] is not None:
+            p['isPatch1'] = 'yes'
+            p['sex1'] = 'yes'
+            p['new_file'] = 'yes'
+        if p['clinical_status'] is not None:
+            p['isPatch1'] = 'yes'
+            p['new_clinical_status'] = 'yes'
+            p['new_file'] = 'yes'
 
 
+# pull cases to update
+update_p1 = rd3tools.find_dict(ped_p1, 'isPatch1', 'yes')
+rd3tools.status_msg('Detected patch1 Cases:', len(update_p1))
+
+len(rd3tools.find_dict(update_p1, 'new_mid', 'yes'))
+len(rd3tools.find_dict(update_p1, 'new_pid', 'yes'))
+len(rd3tools.find_dict(update_p1, 'new_file', 'yes'))
+len(rd3tools.find_dict(update_p1, 'new_clinical_status', 'yes'))
+len(rd3tools.find_dict(update_p1, 'new_sex1', 'yes'))
+
+
+# recode ID
+for d in update_p1:
+    d['patch'] = 'freeze1_original,freeze1_patch1'
+    d['id'] = d['id'] + '_original'
+
+# pull attributes
+update_patch_ids = rd3tools.select_keys(
+    data = update_p1,
+    keys = ['id', 'patch']
+)
+
+# select new mid cases
+update_mid = rd3tools.select_keys(
+    data = rd3tools.find_dict(
+        data = update_p1,
+        attr = 'new_mid',
+        value = 'yes'
+    ),
+    keys = ['id', 'mid']
+)
+
+# select new pid cases
+update_pid = rd3tools.select_keys(
+    data = rd3tools.find_dict(
+        data = update_p1,
+        attr = 'new_pid',
+        value = 'yes'
+    ),
+    keys = ['id', 'pid']
+)
+
+# select new clinical status
+update_clinical = rd3tools.select_keys(
+    data = rd3tools.find_dict(
+        data = update_p1,
+        attr = 'new_clinical_status',
+        value = 'yes'
+    ),
+    keys = ['id', 'clinical_status']
+)
+
+# import
+rd3.batch_update_one_attr(
+    entity = 'rd3_freeze1_subject',
+    attr = 'patch',
+    values = update_patch_ids
+)
+
+rd3.batch_update_one_attr(
+    entity = 'rd3_freeze1_subject',
+    attr = 'mid',
+    values = update_mid
+)
+
+rd3.batch_update_one_attr(
+    entity = 'rd3_freeze1_subject',
+    attr = 'pid',
+    values = update_pid
+)
+
+rd3.batch_update_one_attr(
+    entity = 'rd3_freeze1_subject',
+    attr = 'clinical_status',
+    values = update_clinical
+)
+
+
+# write csv
+def write_csv(path, data):
+    import csv
+    headers = list(data[0].keys())
+    with open(path, 'w') as file:
+        writer = csv.DictWriter(file, fieldnames = headers)
+        writer.writeheader()
+        writer.writerows(data)
+    file.close()
+
+write_csv(
+    path = 'data/patch1_updates.csv',
+    data = rd3tools.select_keys(
+        data = update_p1,
+        keys = [
+            'id',
+            'subjectID',
+            'fid',
+            'mid',
+            'pid',
+            'sex1',
+            'clinical_status',
+            'upload',
+            'isPatch1',
+            'new_mid',
+            'new_pid',
+            'new_file',
+            'new_clinical_status',
+            'new_sex1',
+            'patch'
+        ]
+    )
+)

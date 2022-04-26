15ffa659a105f75053b5f87bac25df26676ff97a
davidruvolo51 <davidruvolo51@gmail.com>
2022-04-26 16:21:51 +0200

    added ped file tools; init phenopacket tools

--- rd3/utils/utils.py
+++ rd3/utils/utils.py
@@ -2,7 +2,7 @@
 #' FILE: utils.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2022-04-25
-#' MODIFIED: 2022-04-25
+#' MODIFIED: 2022-04-26
 #' PURPOSE: misc RD3 tools
 #' STATUS: stable
 #' PACKAGES: **see below**
@@ -11,6 +11,8 @@
 
 from datetime import datetime
 import numpy as np
+import pytz
+import re
 
 def statusMsg(*args):
     """Status Message
@@ -38,3 +40,226 @@ def dtFrameToRecords(data):
     @return list of dictionaries
     """
     return data.to_pandas().replace({np.nan: None}).to_dict('records')
+
+
+def recodeValue(mappings: None, value: str=None, label:str=None, warn=True):
+    """Recode value
+    Recode values using new mappings. It is recommended to define all
+    mappings using lowercase letters and the the input for value should
+    also be lowered.
+    
+    @param mappings a datatable object containing where each key
+        corresponds to a new value
+    @param value string containing a value to recode
+    @param label string that indicates the mapping type for error messages
+    @param warn If True (default), a message will be displayed when a value
+        cannot be mapped
+        
+    @examples
+        mappings = {'First Name': 'firstname', 'Last Name': 'surname'}
+        recodeValue(mappings=mappings, value='Last Name', label='Name)
+    
+    @return string or NoneType
+    """
+    try:
+        return mappings[value]
+    except:
+        if bool(value) and warn:
+            error = f'Error in{label if label else " "}recoding: \"{value}\" not found'
+            statusMsg(error)
+        return None
+
+
+def timestamp(tz='Europe/Amsterdam', timeFormat='%Y-%m-%d'):
+    """Timestamp
+    Return a timestamp specific to user's tz and desired time format
+
+    @param tz string containing a timezone
+    @param timeFormat string indicating how to format the output
+
+    @examples
+        timestamp()
+
+    @return datetime object
+    """
+    return datetime.now(tz=pytz.timezone(tz)).strftime(timeFormat)
+
+
+def toKeyPairs(data, keyAttr:str=None, valueAttr:str=None):
+    """To Key Pairs
+    Convert a list of dictionaries into a key-value dictionary. This method
+    is useful for creating objects for mapping tables.
+    
+    @param data list of dictionaries that you wish to convert
+    @param keyAttr attribute that will be key
+    @param valueAttr attribute that contains the value
+    
+    @return a dictionary
+    """
+    maps = {}
+    for d in data:
+        maps[d[keyAttr]] = d.get(valueAttr)
+    return maps
+
+
+class pedtools:
+    """Pedigree File Tools"""
+    
+    @staticmethod
+    def recodeSexCodes(value):
+        """Recode Sex Code
+        Ped files use the code 1, 2, or other to indicate patient's sex.
+        Recode these values into RD3 terminology.
+        
+        @param value string containing a sex code to recode
+        @return string containing an RD3 sex code
+        """
+        sexCodeMappings = {'1': 'M', '2': 'F', 'other': 'U'}
+        return recodeValue(mappings=sexCodeMappings, value=value, label='PED Sex Code')
+    
+    @staticmethod
+    def recodeAffectedStatus(value):
+        """Recode Affected Status
+        Recode PED code for affected status into RD3 terminology
+        
+        @param value string containing a value to recode
+        @return string containing an RD3 affected status code
+        """
+        affectedStatusMappings={'-9': None, '0': None, '1': False, '2': True}
+        return recodeValue(
+            mappings=affectedStatusMappings,
+            value=value,
+            label='Affected Status'
+        )
+    
+    @staticmethod
+    def parseFileRow(row:dict=None):
+        """Parse Pedigree File Row
+        Transform a row from a PED file into RD3 terminology and shape
+        
+        @param row dictionary that is a row from a PED file
+        @return dictionary
+        """
+        return {
+            'id': row[1],
+            'subjectID': row[1],
+            'fid': row[0],
+            'mid': row[3],
+            'pid': row[2],
+            'sex1': pedtools.recodeSexCodes(value=row[4]),
+            'clinical_status': pedtools.recodeAffectedStatus(value=row[5]),
+            'upload': True
+        }
+        
+    @staticmethod
+    def validatedFileRow(row:dict=None, ids:list=None):
+        """Validate PED Row
+        Validate a single line of data extracted from a PED file
+        
+        @param data a dict containing a single line of data from a PED file.
+            This is the output from `pedtools.parseFileRow`
+        @param ids a list of IDs to compare to
+        @param a dictionary containing validated data
+        
+        @return a dictionary
+        """
+        line = row
+        if ('FAM' in line['id']) or (line['id'] not in ids):
+            statusMsg(
+                'ID {} from family {} does not exist'
+                .format(line['id'], line['fid'])
+            )
+            line['upload'] = False
+        if ('FAM' in line['mid']) or (line['mid'] == '0') or (line['mid'] not in ids) :
+                line['error_mid'] = line['mid']
+                statusMsg(
+                    'removed mid {} as it starts with `FAM`, is `0`, or it does not exist'
+                    .format(line['mid'])
+                )
+                line['mid']=None
+        if ('FAM' in line['pid']) or (line['pid'] == '0') or (line['pid'] not in ids):
+                line['error_pid'] = line['pid']
+                statusMsg(
+                    'removed pid {} as it starts with `FAM`, is `0`, or it does not exist'
+                    .format(line['pid'])
+                )
+                line['pid']=None
+        return line
+    
+    @staticmethod
+    def parseFileContents(contents, ids, filename):
+        """Extract contents from a PED file
+    
+        Extract the contents of a pedigree file
+        
+        @param contents output from `cluster_read_file`
+        @param ids a list of reference IDs to check against
+        @param filename string containing the name of the file (for validation)
+        
+        @return list of dictionaries
+        """
+        data = []
+        for line in contents:
+            d = line.split()
+            if len(d) == 6:
+                raw_line_data = pedtools.parseFileRow(line = d)
+                proc_line_data = pedtools.validatedFileRow(
+                    data = raw_line_data,
+                    ids = ids
+                )
+                data.append(proc_line_data)
+            else:
+                statusMsg(
+                    'Line in {} does not have 6 columns. Has {}'
+                    .format(filename, len(d))
+                )
+        return data
+
+class phenotools:
+    """Phenopacket Tools"""
+    
+    @staticmethod
+    def recodeSexCodes(value):
+        """Recode Phenopacket set value
+        Record phenopackets sex values into RD3 terminology
+        
+        @param value string containing a sex code to recode
+        @return string containing an RD3 sex code
+        """
+        mappings={'female': 'F', 'male': 'M', 'unknown_sex': 'U'}
+        return recodeValue(mappings=mappings, value=value, label="Sex Code")
+    
+    @staticmethod
+    def formatDate(value):
+        """Format date
+        
+        @param value string containing date to recode
+        @return date string in yyyy-mm-dd format
+        """
+        try:
+            return re.sub(r'(T00:00:00Z', '', value).split('-')[0]
+        except:
+            return value
+            
+    @staticmethod
+    def unpackPhenotypicFeatures(data):
+        """Unpack Phenotypic Features data
+        Extract `phenotypicFetures` and separate into 'observed' and
+        'unobserved' phenotypes.
+        
+        @param data output from data['phenopacket']['phenotypicFeatures']
+        @return dictionary with lists for observed and unobserved phenotypes
+        """
+        result = {'phenotype': [], 'hasNotPhenotype': []}
+        for row in data:
+            hpoId= re.sub(r'^(HP:)', 'HP_', row['type']['id'])
+            if not (hpoId in result['phenotype']) and not (hpoId in result['hasNotPhenotype']):
+                if row.get('negated'):
+                    if row['negated']:
+                        result['hasNotPhenotype'].append(hpoId)
+                    if not row['negated']:
+                        result['phenotype'].append(hpoId)
+                else:
+                    result['phenotype'].append(hpoId)
+        return result
+        

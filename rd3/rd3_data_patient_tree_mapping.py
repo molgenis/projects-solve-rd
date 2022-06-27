cc36f2b8bb934296482fd33a3c051952d328c526
davidruvolo51 <davidruvolo51@gmail.com>
2022-06-27 16:14:24 +0200

    updated model; restructured data mapping

--- rd3/rd3_data_patient_tree_mapping.py
+++ rd3/rd3_data_patient_tree_mapping.py
@@ -2,7 +2,7 @@
 #' FILE: rd3_data_patient_tree_mapping.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2022-06-20
-#' MODIFIED: 2022-06-20
+#' MODIFIED: 2022-06-27
 #' PURPOSE: mapping script for patient tree dataset
 #' STATUS: experimental
 #' PACKAGES: datatable, json
@@ -11,6 +11,7 @@
 
 from datatable import dt
 from rd3.api.molgenis import Molgenis
+# from tqdm import tqdm
 import json
 
 def getExperiment(table,sample):
@@ -36,18 +37,23 @@ pwd=environ['MOLGENIS_ACC_PWD']
 rd3 = Molgenis(host)
 rd3.login(usr, pwd)
 
+#//////////////////////////////////////////////////////////////////////////////
+
+# ~ 0 ~
+# Prepare Tree Data
 
 # pull example data (a more complex example)
+# remove `q` and `num` when compiling the full batch
 data = rd3.get(
   entity = 'rd3_overview',
-  attributes = "subjectID,samples",
+  attributes = "subjectID,fid,samples",
   q="patch%3Din%3D(freeze1_original%2Cfreeze2_original%2Cfreeze3_original)%3Bpatch%3Din%3D(noveldeepwes_original%2Cnovellrwgs_original%2Cnovelrnaseq_original%2Cnovelsrwgs_original%2Cnovelwgs_original)",
   num=25
 )
 
 # extract sample identifiers
 patientdata = []
-columnsToIgnore = ['_href', 'subjectID', 'numberOfSamples', 'numberOfExperiments']
+columnsToIgnore = ['_href', 'subjectID', 'fid', 'numberOfSamples', 'numberOfExperiments']
 
 for row in data:
   for column in row.keys():
@@ -55,6 +61,7 @@ for row in data:
       for entry in row[column]:
         newrecord = {
           'subjectID': row['subjectID'],
+          'fid': row.get('fid'),
           'table': entry['_href'].split('/')[3], # get entity id
           'sampleID': entry["id"]
         }
@@ -66,54 +73,71 @@ for row in patientdata:
 
 patientsamples = dt.Frame(patientdata)
 
-# write to json format
-jsonData = {
-  "name": "patient-sample-tree",
-  "children": []
-}
 
+# compile dataset
+# For each subject, create row-level data for the table (i.e., search attributes)
+# and the JSON stringified object. To make things easier, but all information
+# in the json object so you have access to it in the front end
+
+subjectidentifiers = dt.unique(patientsamples[:,'subjectID']).to_list()[0]
+jsonData = []
 counter = 0
-for id in dt.unique(patientsamples[:,'subjectID']).to_list()[0]:
+
+for id in subjectidentifiers:
   patientEntries = patientsamples[dt.f.subjectID == id,:]
   patientJson = {
-    "id": f"RDI-{counter}",
-    "name": patientEntries[0,'subjectID'],
-    "group": "patient"
+    'id': f"RDI-{counter}",
+    'subjectID': patientEntries[0,'subjectID'],
+    'familyID': patientEntries[0,'fid'],
+    'group': 'patient'
   }
   
   # process sample IDs if they exist
   patientSampleIds = dt.unique(patientEntries[:, 'sampleID']).to_list()[0]
   if patientSampleIds:
     samplecounter = 0
-    patientJson["children"] = []
+    patientJson['children'] = []
     for sampleID in patientSampleIds:
       sample = patientEntries[dt.f.sampleID == sampleID, :]
 
       patientSampleJson = {
-        "id":  f"RDI-{counter}.{samplecounter}",
-        "name": sampleID,
-        "group": 'sample' 
+        'id':  f"RDI-{counter}.{samplecounter}",
+        'name': sampleID,
+        'group': 'sample' 
       }
       
       # process experiment IDs if they exist
       patientExperimentIDs = sample['experiment'].to_list()[0]
       if patientExperimentIDs != [None]:
-        patientSampleJson["children"] = []
+        patientSampleJson['children'] = []
         experimentcounter = 0
         for experimentID in patientExperimentIDs:
           if experimentID is not None:
-            patientSampleJson["children"].append({
+            patientSampleJson['children'].append({
               "id": f"RDI-{counter}.{samplecounter}.{experimentcounter}",
               "name": experimentID,
               "group": 'experiment'
             })
             experimentcounter += 1
       samplecounter += 1
-      patientJson["children"].append(patientSampleJson)
+    patientJson['children'].append(patientSampleJson)
+
+  patientRow = {
+    'id': f"RDI-{counter}",
+    'subjectID': patientEntries[0,'subjectID'],
+    'familyID': patientEntries[0,'fid'],
+    'group': 'patient',
+    'json': json.dumps(patientJson)
+  }
 
   counter += 1
-  jsonData["children"].append(patientJson)
+  jsonData.append(patientRow)
   
 
-with open('test.json', 'w') as file:
-  json.dump(jsonData, file)
+#//////////////////////////////////////
+
+# ~ 1 ~
+# Import Data
+
+# rd3.delete('rd3stats_treedata')
+rd3.importData(entity='rd3stats_treedata', data=jsonData)
--- schemas/rd3stats.md
+++ schemas/rd3stats.md
@@ -20,7 +20,8 @@ JSON stringified objects of sample-experiment links per subject
 
 | Name | Label | Description | Data Type |
 |:---- |:-----|:-----------|:---------|
-| subjectID&#8251; | - | An individual who is the subject of personal data, persons to whom data refers, and from whom data are collected, processed, and stored. | string |
+| id&#8251; | - | identifier of the object | string |
+| subjectID | - | An individual who is the subject of personal data, persons to whom data refers, and from whom data are collected, processed, and stored. | string |
 | familyID | - | A domestic group, or a number of domestic groups linked through descent (demonstrated or stipulated) from a common ancestor, marriage, or adoption. | string |
 | json | - | json stringified object containing sample-experiment links | text |
 

f475944e7e7ae358259bb2ca0ada2cf3b77e6b5d
davidruvolo51 <davidruvolo51@gmail.com>
2022-06-27 16:51:45 +0200

    init error box component

---
+++ app/src/components/Errorbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error-box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-box'
+}
+</script>
+
+<style lang="scss" scoped>
+.error-box {
+  background-color: hsl(1, 100%, 95%);
+  color: hsl(10, 90%, 55%);
+  box-sizing: padding-box;
+  padding: 1.06em;
+}
+</style>

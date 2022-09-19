7df71f34af04ebf689077576dc50d18288a227e7
davidruvolo51 <davidruvolo51@gmail.com>
2022-09-19 16:23:31 +0200

    init working version

--- rd3/rd3_cluster_list_analysis_files_processing.py
+++ rd3/rd3_cluster_list_analysis_files_processing.py
@@ -2,42 +2,59 @@
 # FILE: rd3_cluster_list_analysis_files_processing.py
 # AUTHOR: David Ruvolo
 # CREATED: 2022-09-15
-# MODIFIED: 2022-09-15
+# MODIFIED: 2022-09-19
 # PURPOSE: Read compiled csv and prep for import into Molgenis
-# STATUS: in.progress
-# PACKAGES: NA
+# STATUS: stable
+# PACKAGES: Molgenis2, clustertools, datatable, dotenv, os, re
 # COMMENTS: NA
 #///////////////////////////////////////////////////////////////////////////////
 
 from rd3.api.molgenis2 import Molgenis
 from rd3.utils.clustertools import clustertools
-from datatable import dt, f
+from datatable import dt, f, fread
 from dotenv import load_dotenv
 from os import environ
 import re
 
 load_dotenv()
-
 rd3 = Molgenis(environ['MOLGENIS_ACC_HOST'])
 rd3.login(environ['MOLGENIS_ACC_USR'], environ['MOLGENIS_ACC_PWD'])
 
-path='/groups/solve-rd/tmp10/dcruvolo/rd3_snv-indel_contents.csv'
-data = dt.Frame(clustertools.readCsv(path))
+# get known file types
+knownFileTypes = dt.Frame(rd3.get('rd3_cluster_filetypes'))['value'].to_list()[0]
+
+# read data
+cluster = clustertools('airlock+gearshift')
+path = '/home/umcg-druvolo/data/rd3_cnv_contents.csv'
+data = dt.Frame(cluster.readCsv(path))
 
 
-def extractErn(value):
+def extractPattern(value):
   """Match Pattern Dictionary
   Test a dictionary of patterns and return a value
   
+  @param patterns a search pattern
   @param value string to search
-  @param patterns a dictionary where each key is as pattern
   """
+  mappings = {
+    'ERNRITA': 'ern_rita',
+    'GENTURIS': 'ern_genturis',
+    'ITHACA': 'ern_ithaca',
+    'NMD': 'ern_euro_nmd',
+    'RND': 'ern_rnd',
+    'RITA': 'ern_rita'
+  }
   search = re.search(
-    pattern = r'(([/_-]?(nmd|genturis|epicare|ithaca|rita|rnd)[/_]?)|ern-rita)',
+    r'(([/_-]?(nmd|genturis|epicare|ithaca|rita|rnd)[/_]?)|ern-rita)',
     string = value.lower()
   )
   if search:
-    return re.sub(r'([/_-])','', search.group()).upper()
+    match = re.sub(r'([/_-])','', search.group()).upper() 
+    try:
+      return mappings[match]
+    except KeyError as error:
+      print(error)
+      return match
   else:
     return None
 
@@ -48,6 +65,8 @@ def recodeFileExtension(value):
     return 'hcdiff'
   if re.search(r'(.vcf|.vcf.gz)', value):
     return 'vcf'
+  if re.search(r'(.txt.tmp)', value):
+    return 'txt'
 
 
 #///////////////////////////////////////
@@ -58,7 +77,8 @@ def recodeFileExtension(value):
 # filter data:
 #  - remove all sftp files
 #  - remove all files where the size is 0
-fileData = data[dt.re.match(f.path, '.*/sftp/.*')==False, :][f.size != '0', :]
+fileData = data[dt.re.match(f.path, '.*/sftp/.*')==False, :]
+fileData = fileData[f.size != '0', :]
 
 
 # ~ 0a ~
@@ -81,33 +101,57 @@ fileData['inode'] = dt.Frame([
   for tuple in fileData[:, (f.inode, f.cumulativeCount)].to_tuples()
 ])
 
+# check unique IDs against total rows once more
 dt.unique(fileData['inode']).nrows == fileData.nrows
 
 
 # ~ 0b ~
 # Extract ERN from file path
 fileData['ern'] = dt.Frame([
-  extractErn(d)
+  extractPattern(d)
   for d in fileData['path'].to_list()[0]
 ])
 
+# check mappings: all ern values should be 'ern_*'
 # fileData[:, (f.ern,f.path)]
+# fileData[f.ern==None, (f.ern,f.path)]
 # dt.unique(fileData['ern'])
 # fileData[:, dt.count(), dt.by(f.ern)]
 # fileData[:, dt.count(), dt.by(f.ern)][:, dt.sum(f.count)]
 
+
 # ~ 0c ~
 # Check file extensions
-dt.unique(fileData[:, f.extension])
-
-acceptedFiles = [
-  'csv',
-  'hcdiff','list','log','metainfo','txt','tsv','xlsx'
-]
 
 fileData['extension'] = dt.Frame([
+  type.lower() if type is not None else type
+  for type in fileData['extension'].to_list()[0]
+])
+
+# check to see which files are known. Investigate/add new types
+uniqueFileTypes = dt.unique(fileData[:, f.extension])
+uniqueFileTypes['extension'] = dt.Frame([
+  type if type is not None else type
+  for type in uniqueFileTypes['extension'].to_list()[0]
+])
+
+uniqueFileTypes['isKnown'] = dt.Frame([
+  type in knownFileTypes
+  for type in uniqueFileTypes['extension'].to_list()[0]
+])
+
+uniqueFileTypes[f.isKnown == False, :]
+
+# for new file add them here and import
+# fileData[f.extension=='tbi','name']
+# filetypes = dt.Frame(value=['err', 'hdf5', 'log'])
+# rd3.importDatatableAsCsv('rd3_cluster_filetypes', filetypes)
+# knownFileTypes.extend(filetypes['value'].to_list()[0])
+
+# recode known variations
+fileData['extension'] = dt.Frame([
   recodeFileExtension(tuple[1])
-  if tuple[0] not in acceptedFiles else tuple[0]
+  if tuple[0] not in knownFileTypes else tuple[0]
   for tuple in fileData[:, ['extension', 'name']].to_tuples()
 ])
 
@@ -118,8 +162,10 @@ fileData['extension'] = dt.Frame([
 # ~ 1 ~
 # Import
 
-# rd3.delete('rd3_cluster_results_snvindel')
-rd3.importDatatableAsCsv(
-  pkg_entity='rd3_cluster_results_snvindel',
-  data = fileData
-)
+# rd3.delete('rd3_cluster_results_')
+
+# alternatively save to csv and import
+# fileData.to_csv('data/rd3_cluster_results_str.csv')
+# fileData = fread('data/rd3_cluster_results_cnv.csv')
+
+rd3.importDatatableAsCsv('rd3_cluster_results_str', fileData)

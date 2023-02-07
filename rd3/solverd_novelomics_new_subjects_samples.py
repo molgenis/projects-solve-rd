146b3dad1545ffd436d6f239564a67f174781ecb
davidruvolo51 <davidruvolo51@gmail.com>
2023-02-07 15:16:13 +0100

    added flatten dataset method

--- rd3/solverd_novelomics_new_subjects_samples.py
+++ rd3/solverd_novelomics_new_subjects_samples.py
@@ -22,45 +22,9 @@ from rd3.utils.utils import (
   dtFrameToRecords,
   timestamp,
   toKeyPairs,
-  recodeValue
+  recodeValue,
+  flattenDataset
 )
-
-def flattenDataset(data, columnPatterns=None):
-  """Flatten Dataset
-  Flatten all nested attributes in a recordset based on a specific column names.
-  
-  @param data a recordset
-  @param column string containing row headers to detect: "subjectID|id|value"
-  @return a new recordset containing flattened data
-  """
-  newData = data
-  for row in tqdm(newData):
-    if '_href' in row:
-      del row['_href']
-    for column in row.keys():
-      if isinstance(row[column], dict):
-        if bool(row[column]):
-          columnMatch = re.search(columnPatterns, ','.join(row[column].keys()))
-          if bool(columnMatch):
-            row[column] = row[column][columnMatch.group()]
-          else:
-            print(f'Variable {column} is type "dict", but no target column found')
-        else:
-          row[column] = None
-      if isinstance(row[column], list):
-        if bool(row[column]):
-          values = []
-          for nestedrow in row[column]:
-            columnMatch = re.search(columnPatterns, ','.join(nestedrow.keys()))
-            if bool(columnMatch):
-              values.append(nestedrow[columnMatch.group()])
-            else:
-              print(f'Variable {column} is type "list", but no target column found')
-          if bool(values):
-            row[column] = ','.join(values)
-        else:
-          row[column] = None
-  return newData
 
 def getWrappedValues(value):
   """Get Wrapped Values
--- rd3/utils/utils.py
+++ rd3/utils/utils.py
@@ -2,7 +2,7 @@
 # ' FILE: utils.py
 # ' AUTHOR: David Ruvolo
 # ' CREATED: 2022-04-25
-# ' MODIFIED: 2022-05-30
+# ' MODIFIED: 2023-02-07
 # ' PURPOSE: misc RD3 tools
 # ' STATUS: stable
 # ' PACKAGES: **see below**
@@ -10,6 +10,7 @@
 # '////////////////////////////////////////////////////////////////////////////
 
 from datetime import datetime
+from tqdm import tqdm
 import numpy as np
 import pytz
 import re
@@ -111,6 +112,42 @@ def flattenBoolArray(array, keepTrueValues=True):
   else:
     return None
 
+def flattenDataset(data, columnPatterns=None):
+  """Flatten Dataset
+  Flatten all nested attributes in a recordset based on a specific column names.
+  
+  @param data a recordset
+  @param column string containing row headers to detect: "subjectID|id|value"
+  @return a new recordset containing flattened data
+  """
+  newData = data
+  for row in tqdm(newData):
+    if '_href' in row:
+      del row['_href']
+    for column in row.keys():
+      if isinstance(row[column], dict):
+        if bool(row[column]):
+          columnMatch = re.search(columnPatterns, ','.join(row[column].keys()))
+          if bool(columnMatch):
+            row[column] = row[column][columnMatch.group()]
+          else:
+            print(f'Variable {column} is type "dict", but no target column found')
+        else:
+          row[column] = None
+      if isinstance(row[column], list):
+        if bool(row[column]):
+          values = []
+          for nestedrow in row[column]:
+            columnMatch = re.search(columnPatterns, ','.join(nestedrow.keys()))
+            if bool(columnMatch):
+              values.append(nestedrow[columnMatch.group()])
+            else:
+              print(f'Variable {column} is type "list", but no target column found')
+          if bool(values):
+            row[column] = ','.join(values)
+        else:
+          row[column] = None
+  return newData
 
 def flattenStringArray(array):
   """Flatten String Array

d7f439404cc1e19b601717bed58ae705ec8bc223
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-22 15:48:54 +0100

    added pandas import

--- rd3/api/molgenis2.py
+++ rd3/api/molgenis2.py
@@ -2,7 +2,7 @@
 #' FILE: molgenis2.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2022-07-28
-#' MODIFIED: 2022-12-07
+#' MODIFIED: 2023-03-22
 #' PURPOSE: molgenis.client extensions for DataTable
 #' STATUS: stable
 #' PACKAGES: **see below**
@@ -10,7 +10,7 @@
 #'////////////////////////////////////////////////////////////////////////////
 
 import molgenis.client as molgenis
-from os.path import abspath
+from os import path
 import numpy as np
 import datetime
 import requests
@@ -19,8 +19,8 @@ import pytz
 import json
 import csv
 
-def now():
-  return datetime.datetime.now(tz=pytz.timezone('Europe/Amsterdam')).strftime('%H:%M:%S.%f')[:-3]
+def now(tz='Europe/Amsterdam'):
+  return datetime.datetime.now(tz=pytz.timezone(tz)).strftime('%H:%M:%S.%f')[:-3]
 
 def print2(*args):
   """Status Message
@@ -32,11 +32,22 @@ def print2(*args):
   print(f'[{now()}] {message}')
 
 class Molgenis(molgenis.Session):
+  """Extensions of the molgenis.client
+  The Molgenis class allows you to import Pandas or DataTable objects using
+  the File Import API rather than the REST API. This allows you to import
+  datasets larger than 1000 entities.
+  """
   def __init__(self, *args, **kwargs):
     super(Molgenis, self).__init__(*args, **kwargs)
     self.fileImportEndpoint = f"{self._root_url}plugin/importwizard/importFile"
+    
+  def __checkFileImport__(self, pkg_entity, response):
+    if (response.status_code // 100 ) != 2:
+      print2('Failed to import data into',pkg_entity,'(',response.status_code,')')
+    else:
+      print2('Imported data into', pkg_entity)
   
-  def _datatableToCsv(self, path, datatable):
+  def __datatableToCsv__(self, path, datatable):
     """To CSV
     Write datatable object as CSV file
 
@@ -45,54 +56,93 @@ class Molgenis(molgenis.Session):
     """
     data = datatable.to_pandas().replace({np.nan: None})
     data.to_csv(path, index=False, quoting=csv.QUOTE_ALL)
+    
+  def __dfToCsv__(self, path, df):
+    """To CSV
+    Write pandas dataframe as CSV file
+
+    @param path location to save the file
+    @param df pandas data.frame
+    """
+    data = df.replace({np.nan: None})
+    data.to_csv(path, index=False, quoting=csv.QUOTE_ALL)
   
   def importDatatableAsCsv(self, pkg_entity: str, data):
     """Import Datatable As CSV
     Save a datatable object to as csv file and import into MOLGENIS using the
     importFile api.
     
-    @param pkg_entity table identifier in emx format (package_entity)
+    NOTE: The File API will return a response if the file was imported. The
+    response is NOT an indicator that the file is valid. Please check the
+    entity sys_Import for more information.
+    
+    @param pkg_entity table identifier in emx format: package_entity
     @param data a datatable object
-    @param label a description to print (e.g., table name)
+    
+    @return status message
     """
     with tempfile.TemporaryDirectory() as tmpdir:
       filepath=f"{tmpdir}/{pkg_entity}.csv"
-      self._datatableToCsv(filepath, data)
-      with open(abspath(filepath),'r') as file:
+      self.__datatableToCsv__(filepath, data)
+      with open(path.abspath(filepath), 'r') as file:
         response = self._session.post(
           url = self.fileImportEndpoint,
           headers = self._headers.token_header,
           files = {'file': file},
           params = {'action': 'add_update_existing', 'metadataAction': 'ignore'}
         )
-        if (response.status_code // 100 ) != 2:
-          print2('Failed to import data into', pkg_entity, '(', response.status_code, ')')
-        else:
-          print2('Imported data into', pkg_entity)
+        self.__checkFileImport__(pkg_entity=pkg_entity, response=response)
         return response
-  
+      
+  def importPandasAsCsv(self, pkg_entity, data):
+    """Import Pandas data.frame As CSV
+    Save a datatable object to as csv file and import into MOLGENIS using the
+    importFile api.
+    
+    NOTE: The File API will return a response if the file was imported. The
+    response is NOT an indicator that the file is valid. Please check the
+    entity sys_Import for more information.
+    
+    @param pkg_entity table identifier in emx format: package_entity
+    @param data a python data.frame
+    
+    @return status message
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+      filepath=f"{tmpdir}/{pkg_entity}.csv"
+      self.__dfToCsv__(filepath, data)
+      with open(path.abspath(filepath), 'r') as file:
+        response = self._session.post(
+          url=self.fileImportApi,
+          headers = self._headers.token_header,
+          files={'file': file},
+          params = {'action': 'add_update_existing', 'metadataAction': 'ignore'}
+        )
+        self.__checkFileImport__(pkg_entity=pkg_entity, response=response)
+        return response
+
   def batchUpdate(self, pkg_entity, column, data):
-      """Batch Update
-      Batch update a column in a table for 1000+ entities. Smaller datasets also work too.
-      
-      @param pkg_entity table identifier in emx format (package_entity)
-      @param column name of column to update
-      @param data a recordset to import
-      
-      @return a response code
-      """
-      url=f"{self._root_url}api/v2/{pkg_entity}/{column}"
-      headers=self._headers.ct_token_header
-      
-      batches = range(0, len(data), 1000)
-      print(f"Importing {len(batches)} batch{'es'[:len(batches)^1]} into {pkg_entity}${column}")
-      for batch in batches:
-        batchNumber = round(batch / 1000, ndigits=None) + 1
-        batchToImport = json.dumps({'entities': data[batch:batch+1000] })
-        response = self._session.put(url, headers=headers, data=batchToImport)
-        try:
-          if response.status_code // 100 == 2:
-            print(f'Batch {batchNumber}: Imported {len(data)} entities')
-          response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-          print(f'Batch {batchNumber} Error: unable to import data:\n{str(err)}')
+    """Batch Update
+    Batch update a column in a table for 1000+ entities. Smaller datasets also work too.
+    
+    @param pkg_entity table identifier in emx format (package_entity)
+    @param column name of column to update
+    @param data a recordset to import
+    
+    @return a response code
+    """
+    url=f"{self._root_url}api/v2/{pkg_entity}/{column}"
+    headers=self._headers.ct_token_header
+    
+    batches = range(0, len(data), 1000)
+    print(f"Importing {len(batches)} batch{'es'[:len(batches)^1]} into {pkg_entity}${column}")
+    for batch in batches:
+      batchNumber = round(batch / 1000, ndigits=None) + 1
+      batchToImport = json.dumps({'entities': data[batch:batch+1000] })
+      response = self._session.put(url, headers=headers, data=batchToImport)
+      try:
+        if response.status_code // 100 == 2:
+          print(f'Batch {batchNumber}: Imported {len(data)} entities')
+        response.raise_for_status()
+      except requests.exceptions.HTTPError as err:
+        print(f'Batch {batchNumber} Error: unable to import data:\n{str(err)}')

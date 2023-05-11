3007d4f25a9585addfb4f668cd3f3aef59c1d45d
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-11 11:47:59 +0200

    restructured files

---
+++ emx2/test/test.py
@@ -0,0 +1,55 @@
+#///////////////////////////////////////////////////////////////////////////////
+# FILE: test.py
+# AUTHOR: David Ruvolo
+# CREATED: 2023-05-11
+# MODIFIED: 2023-05-11
+# PURPOSE: test EMX2 script for emx2 client
+# STATUS: stable
+# PACKAGES: **see below**
+# COMMENTS: import xlsx file into a emx2 instance
+#///////////////////////////////////////////////////////////////////////////////
+
+from emx2.api.emx2 import Molgenis as EMX2
+from dotenv import load_dotenv
+from os import environ
+import random
+load_dotenv()
+
+# connect to database
+emx2 = EMX2(environ['MOLGENIS_EMX2_HOST'])
+emx2.signin(environ['MOLGENIS_EMX2_USR'],environ['MOLGENIS_EMX2_PWD'])
+
+
+# create example data
+data =[
+  {'commonName': 'test-560'},
+  {'commonName': 'test-561'},
+  {'commonName': 'test-562'},
+  {'commonName': 'test-563'},
+  {'commonName': 'test-564'},
+  {'commonName': 'test-565'},
+  {'commonName': 'test-566'},
+  {'commonName': 'test-567'},
+  {'commonName': 'test-568'},
+  {'commonName': 'test-569'},
+  {'commonName': 'test-570'},
+]
+
+# import
+emx2.add(database='BirdDataAU', table='Species', data=data)
+
+# add columns
+for row in data:
+  row['count'] = random.randrange(100, 1000)
+  row['reportingRate'] = random.random()
+  row['reportingLocation'] = {'name': 'AU'}
+
+# update
+emx2.update(database='BirdDataAU', table='Species', data=data)
+
+# pull query
+
+
+# delete records
+emx2.delete(database='BirdDataAU', table='Species', data=data)
+

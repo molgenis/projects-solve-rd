e7ce9c813aab8a3541ec14b3537cef67aad0e05f
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-11 11:48:24 +0200

    initial working client

--- emx2/api/emx2.py
+++ emx2/api/emx2.py
@@ -2,7 +2,7 @@
 #' FILE: molgenis_emx2_client.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2022-02-09
-#' MODIFIED: 2022-02-23
+#' MODIFIED: 2023-05-11
 #' PURPOSE: EMX2 API py client
 #' STATUS: in.progress
 #' PACKAGES: requests
@@ -10,14 +10,14 @@
 #'////////////////////////////////////////////////////////////////////////////
 
 from urllib.parse import urlparse, urlunparse
-from rd3.api.emx2.cli import cli
-import rd3.api.emx2.graphql as graphql
+from emx2.api.graphql import graphql
+from emx2.api.cli import cli
 import requests
 import re
 
 print2 = cli()
 
-def cleanUrl(string):
+def cleanUrl(string:str=None):
   """Clean Urls
   @param url (str) : string containing a URL
   """
@@ -26,7 +26,7 @@ def cleanUrl(string):
   return urlunparse(value)
 
 class Molgenis:
-  def __init__(self, url):
+  def __init__(self, url:str=None):
     self.host = cleanUrl(url)
     self.session = requests.Session()
     
@@ -34,40 +34,19 @@ class Molgenis:
     """POST Wrapper
     Send a POST request to an EMX2 instance
 
-    @param *args other parameters to pass down to session.post    
+    @param *kwargs other parameters to pass down to session.post    
     @return response object
     """
-    response = self.session.post(**kwargs)
-    response.raise_for_status()
+    try:
+      response = self.session.post(**kwargs)
+      response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+      errors = '\n'.join([err['message'] for err in response.json()['errors']])
+      print2.alert_error(print2.text_error('ERROR:\n', errors))
     return response
-  
-  def __validateDataResponse__(self,response):
-    json = response.json()
-    if response.status_code == 200:
-      return json.get('data')
-    else:
-      print2.alert_error(
-        'Failed to retrieve data\n', print2.text_error('ERROR:\n'),
-        print2.text_error(json.get('errors')[0].get('message'))
-      )
-   
-  def __validateImportResponse__(self,response,database,table):
-    """Validate Import Response
-    Make sure all import posts are valid
-    
-    @return response object
-    """
-    location=print2.text_value(database, '::', table)
-    if response.status_code == 200:
-      print2.alert_success('Imported data into',location)
-    else:
-      error = response.json().get('errors')[0].get('message')
-      print2.alert_error(
-        'Failed to import data into', location,
-        print2.text_error('\nError:\n  ', error)
-      ) 
 
-  def signin(self, username, password):
+
+  def signin(self, username:str=None, password:str=None):
     """Signin
     Sign in into an EMX2 instance with your username and password
     
@@ -80,7 +59,7 @@ class Molgenis:
     response = self._post(
       url=f'{self.host}/apps/central/graphql',
       json={
-        'query': graphql.signin,
+        'query': graphql.signin(),
         'variables': {'email': username, 'password': password}
       }
     )
@@ -91,16 +70,94 @@ class Molgenis:
     if data.get('status') == 'SUCCESS':
       print2.alert_success('Signed into', msg)
       self.token = data.get('token')
-    else:
-      print2.alert_error(
-        'Unable to sign into', msg, '\n',
-        print2.text_error('ERROR:'), '\n  ',
-        print2.text_error(data.get('message'))
-      )
     return response
 
+
+  def add(self, database:str=None, table:str=None, data:list=[]):
+    """Add Data
+    Import one or more records into a table. Row identifiers are required.
+    
+    @param database name of the database
+    @param table name of the table to import data into
+    @param data list of one or more dictionaries
+    
+    @return response
+    """
+    response = self._post(
+      url=f"{self.host}/{database}/api/graphql",
+      json={
+        'query': graphql.insert(table=table),
+        'variables': {'records': data}
+      }
+    )
+
+    if response.status_code==200:
+      print2.alert_success(
+        'Added', print2.text_value(len(data)),
+        f"record{'s'[:len(data)^1]} into",
+        print2.text_value(f"{database}::{table}")
+      )
+
+    return response
+
+
+  def delete(self, database:str=None, table:str=None, data:list=[]):
+    """Delete data
+    Delete one or more records from a table. Row identifiers are required
+    
+    @param database name of the database
+    @param table name of the table to remove records from
+    @param data a list of dictionaries containing the row identifiers
+    
+    @return response
+    """
+    response = self._post(
+      url=f"{self.host}/{database}/api/graphql",
+      json={
+        'query': graphql.delete(table=table),
+        'variables': {'records': data}
+      }
+    )
+
+    if response.status_code==200:
+      print2.alert_success(
+        'Deleted', print2.text_value(len(data)),
+        f"record{'s'[:len(data)^1]} from",
+        print2.text_value(f"{database}::{table}")
+      )
+
+    return response
+
+
+  def update(self, database:str=None, table:str=None, data:list=[]):
+    """Update data
+    Update one or more records from a table. Row identifiers are required
+    
+    @param database name of the database
+    @param table name of the table
+    @param data a list of dictionaries containing the row identifiers
+    
+    @return response
+    """
+    response = self._post(
+      url=f"{self.host}/{database}/api/graphql",
+      json={
+        'query': graphql.update(table=table),
+        'variables': {'records': data}
+      }
+    )
+
+    if response.status_code==200:
+      print2.alert_success(
+        'Updated', print2.text_value(len(data)),
+        f"record{'s'[:len(data)^1]} in",
+        print2.text_value(f"{database}::{table}")
+      )
+
+    return response
   
-  def query(self, database, query):
+  
+  def query(self, database:str=None, query:str=None, variables: dict={}):
     """Query
     Run a graphql query
     
@@ -109,10 +166,18 @@ class Molgenis:
     
     @return json or error message
     """
-    response = self._post(url=f"{self.host}/{database}/api/graphql", json={'query': query})
-    return self.__validateDataResponse__(response)
+    response = self._post(
+      url=f"{self.host}/{database}/api/graphql",
+      json={'query': query, 'variables': variables}
+    )
+
+    if response.status_code == 200:
+      print2.alert_success('Successfully executed query')
+
+    return response
   
-  def getSchema(self, database, table=None):
+  
+  def getSchema(self, database: str=None, table: str=None):
     """Get Schema
     Retrieve the schema of a database
     
@@ -123,15 +188,18 @@ class Molgenis:
     """
     response = self._post(
       url=f"{self.host}/{database}/api/graphql",
-      json={'query': graphql.schema}
+      json={'query': graphql.schema()}
     )
-    data = self.__validateDataResponse__(response)
+    
+    data = response.json()['data']
+
     if (table is not None) and bool(data):
       return [schematable for schematable in data['_schema']['tables'] if schematable['name'] == table][0]
     else:
       return data
       
-  def importCsvFile(self, database, table, file):
+
+  def importCsvFile(self, database:str=None, table:str=None, file:str=None):
     """Import CSV File
     Import a csv file into a table
     
@@ -157,12 +225,21 @@ class Molgenis:
       databinary = stream.read()
       stream.close()
 
-    url=f"{self.host}/{database}/api/csv/{table}"
-    response = self._post(url=url,data=databinary)
-    self.__validateImportResponse__(response=response,database=database,table=table)
+    response = self._post(
+      url=f"{self.host}/{database}/api/csv/{table}",
+      data=databinary
+    )
+    
+    if response.status_code == 200:
+      print2.alert_success(
+        'Import data into',
+        print2.text_value(f"{database}::{table}")
+      )
+    
     return response
-          
-  def importData(self, database, table, data):
+
+    
+  def importData(self, database:str=None, table:str=None, data:str=None):
     """Import Data
     Import a data object into a schema table
 
@@ -194,5 +271,11 @@ class Molgenis:
       headers={'Content-Type': 'text/csv'},
       data=data
     )
-    self.__validateImportResponse__(response=response,database=database,table=table)
+    
+    if response.status_code == 200:
+      print2.alert_success(
+        'Import data into',
+        print2.text_value(f"{database}::{table}")
+      )
+
     return response
--- emx2/api/graphql.py
+++ emx2/api/graphql.py
@@ -2,78 +2,160 @@
 # FILE: graphql.py
 # AUTHOR: David Ruvolo
 # CREATED: 2023-05-10
-# MODIFIED: 2023-05-10
+# MODIFIED: 2023-05-11
 # PURPOSE: commonly used graphql queries
-# STATUS: in.progress
+# STATUS: stable
 # PACKAGES: NA
 # COMMENTS: Import file as 'import path.to.graphql.file as graphql'
 #///////////////////////////////////////////////////////////////////////////////
 
 
-# @name Signin
-# @description Mutation to sign into an EMX2 molgenis instance. Function must include
-# username and password in the posted data.
-#
-# @example
-# ```py
-# import requests
-# requests.post(...,
-#   json={
-#     'query': graphql.query,
-#     'variables': {'username': ..., 'password': ...}
-#   }
-# )
-# 
-# ```
-#
-signin="""
-  mutation($email:String, $password: String) {
-    signin(email: $email, password: $password) {
-      status
-      message
-      token
-    }
-  }
-"""
+class graphql:
+  
+  @staticmethod
+  def signin():
+    # @name Signin
+    # @description Mutation to sign into an EMX2 molgenis instance. Function must include
+    # username and password in the posted data.
+    #
+    # @example
+    # ```py
+    # import requests
+    # requests.post(...,
+    #   json={
+    #     'query': graphql.query,
+    #     'variables': {'username': ..., 'password': ...}
+    #   }
+    # )
+    # 
+    # ```
+    #
+    return """
+      mutation($email:String, $password: String) {
+        signin(email: $email, password: $password) {
+          status
+          message
+        }
+      }
+    """
+  
+  @staticmethod
+  def signout():
+    # @name signout
+    # @description Signout of an EMX2 instance
+    return """
+      mutation {
+        signout {
+          status
+          message
+        }
+      }
+    """
 
-# @name signout
-# @description Signout of an EMX2 instance
-signout="""
-  mutation {
-    signout {
-      status
-      message
-    }
-  }
-"""
-
-# @name schema
-# @description get database schema
-schema=""" {
-  _schema {
-    name
-    tables {
-      name
-      id
-      externalSchema
-      inherit
-      tableType
-      columns {
+  @staticmethod
+  def schema():
+    # @name schema
+    # @description get database schema
+    return """ {
+      _schema {
         name
-        id
-        position
-        columnType
-        key
-        required
-        refTable
-        refLink
-        refBack
-        refLabel
-        validation
-        visible
-        semantics
+        tables {
+          name
+          id
+          externalSchema
+          inherit
+          tableType
+          columns {
+            name
+            id
+            position
+            columnType
+            key
+            required
+            refTable
+            refLink
+            refBack
+            refLabel
+            validation
+            visible
+            semantics
+          }
+        }
       }
     }
-  }
-}
-"""
+    """
+
+  # @name delete
+  # @description query to remove record(s) from a table.
+  # 
+  # @param table name of the table
+  #
+  # @examples
+  # When sending the query, define the post like so. (data should be a dict)
+  #
+  # ```
+  # emx2.delete(
+  #   database='mydatabase',
+  #   query=graphql.delete(table='mytable'),
+  #   variables={'records': [records-to-import]}
+  # )
+  # ```
+  def delete(table:str=None):
+    return (
+      "mutation delete($records:["+ table + "Input]) {\n"
+      "  delete(" + table + ":$records) {\n"
+      "    status\n"
+      "    message\n"
+      "  }\n"
+      "}"
+    )
+  
+  # @name insert
+  # @description query to add record(s) into the database.
+  # 
+  # @param table name of the table to import data into
+  #
+  # @examples
+  # When sending the query, define the post like so. (data should be a dict)
+  #
+  # ```
+  # emx2.insert(
+  #   database='mydatabase',
+  #   query=graphql.insert(table='mytable'),
+  #   variables={'records': [record-to-import]}
+  # )
+  # ```
+  def insert(table:str=None):
+    return (
+      "mutation insert($records:["+ table + "Input]) {\n"
+      "  insert(" + table + ":$records) {\n"
+      "    status\n"
+      "    message\n"
+      "  }\n"
+      "}"
+    )
+
+  # @name update
+  # @description query to update record(s) into the database.
+  # 
+  # @param table name of the table to import data into
+  #
+  # @examples
+  # When sending the query, define the post like so. (data should be a dict)
+  #
+  # ```
+  # emx2.update(
+  #   database='mydatabase',
+  #   query=graphql.update(table='mytable'),
+  #   variables={'records': [record-to-import]}
+  # )
+  # ```
+  def update(table:str=None):
+    return (
+      "mutation update($records:["+ table + "Input]) {\n"
+      "  update(" + table + ":$records) {\n"
+      "    status\n"
+      "    message\n"
+      "  }\n"
+      "}"
+    )

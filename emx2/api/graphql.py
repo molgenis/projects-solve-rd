a72dd9fed4749fd58ce2846f6850010552423b03
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-12 09:02:44 +0200

    added enum for operations and fix docs

--- emx2/api/graphql.py
+++ emx2/api/graphql.py
@@ -2,34 +2,42 @@
 # FILE: graphql.py
 # AUTHOR: David Ruvolo
 # CREATED: 2023-05-10
-# MODIFIED: 2023-05-11
+# MODIFIED: 2023-05-12
 # PURPOSE: commonly used graphql queries
 # STATUS: stable
 # PACKAGES: NA
 # COMMENTS: Import file as 'import path.to.graphql.file as graphql'
 #///////////////////////////////////////////////////////////////////////////////
 
+from enum import Enum
+
+class Operations(Enum):
+  """EMX2 Graphql operations"""
+  insert = "insert"
+  update = "update"
+  save = "save"
+  delete = "delete"
 
 class graphql:
   
   @staticmethod
   def signin():
-    # @name Signin
-    # @description Mutation to sign into an EMX2 molgenis instance. Function must include
-    # username and password in the posted data.
-    #
-    # @example
-    # ```py
-    # import requests
-    # requests.post(...,
-    #   json={
-    #     'query': graphql.query,
-    #     'variables': {'username': ..., 'password': ...}
-    #   }
-    # )
-    # 
-    # ```
-    #
+    """signin
+    Mutation to sign into an EMX2 molgenis instance. Function must include
+    username and password in the posted data.
+    
+    @example
+    ```py
+    import requests
+    requests.post(...,
+      json={
+        'query': graphql.query,
+        'variables': {'username': ..., 'password': ...}
+      }
+    )
+    
+    ```
+    """
     return """
       mutation($email:String, $password: String) {
         signin(email: $email, password: $password) {
@@ -41,8 +49,7 @@ class graphql:
   
   @staticmethod
   def signout():
-    # @name signout
-    # @description Signout of an EMX2 instance
+    """Signout"""
     return """
       mutation {
         signout {
@@ -54,8 +61,9 @@ class graphql:
 
   @staticmethod
   def schema():
-    # @name schema
-    # @description get database schema
+    """schema
+    Default query for schema
+    """
     return """ {
       _schema {
         name
@@ -85,77 +93,79 @@ class graphql:
     }
     """
 
-  # @name delete
-  # @description query to remove record(s) from a table.
-  # 
-  # @param table name of the table
-  #
-  # @examples
-  # When sending the query, define the post like so. (data should be a dict)
-  #
-  # ```
-  # emx2.delete(
-  #   database='mydatabase',
-  #   query=graphql.delete(table='mytable'),
-  #   variables={'records': [records-to-import]}
-  # )
-  # ```
-  def delete(table:str=None):
+  def _operation(operation:str=None, table:str=None):
+    """Operation
+    Wrapper around operations (update, delete, save, insert)
+    
+    @param operation choose insert, update, save, or delete
+    @param table name of the table (passed from parent method)
+    
+    @return string; graphql string
+    """
+    if not hasattr(Operations, operation):
+      raise ValueError('Invalid operation used in query')
+
     return (
-      "mutation delete($records:["+ table + "Input]) {\n"
-      "  delete(" + table + ":$records) {\n"
-      "    status\n"
-      "    message\n"
-      "  }\n"
-      "}"
-    )
-  
-  # @name insert
-  # @description query to add record(s) into the database.
-  # 
-  # @param table name of the table to import data into
-  #
-  # @examples
-  # When sending the query, define the post like so. (data should be a dict)
-  #
-  # ```
-  # emx2.insert(
-  #   database='mydatabase',
-  #   query=graphql.insert(table='mytable'),
-  #   variables={'records': [record-to-import]}
-  # )
-  # ```
-  def insert(table:str=None):
-    return (
-      "mutation insert($records:["+ table + "Input]) {\n"
-      "  insert(" + table + ":$records) {\n"
+      "mutation " + operation + "($records:["+ table + "Input]) {\n"
+      "  " + operation + "(" + table + ":$records) {\n"
       "    status\n"
       "    message\n"
       "  }\n"
       "}"
     )
 
-  # @name update
-  # @description query to update record(s) into the database.
-  # 
-  # @param table name of the table to import data into
-  #
-  # @examples
-  # When sending the query, define the post like so. (data should be a dict)
-  #
-  # ```
-  # emx2.update(
-  #   database='mydatabase',
-  #   query=graphql.update(table='mytable'),
-  #   variables={'records': [record-to-import]}
-  # )
-  # ```
-  def update(table:str=None):
-    return (
-      "mutation update($records:["+ table + "Input]) {\n"
-      "  update(" + table + ":$records) {\n"
-      "    status\n"
-      "    message\n"
-      "  }\n"
-      "}"
+  def delete(table:str=None):
+    """Delete    
+    Query to remove record(s) from a table.
+    
+    @param table name of the table
+    
+    @examples
+    When sending the query, define the post like so. (data should be a dict)
+        ```
+    emx2.delete(
+      database='mydatabase',
+      query=graphql.delete(table='mytable'),
+      variables={'records': [records-to-import]}
     )
+    ```
+    """
+    return graphql._operation(operation='delete', table=table)
+  
+  def insert(table:str=None):
+    """insert
+    Query to add record(s) into the database.
+    
+    @param table name of the table to import data into
+   
+    @examples
+    When sending the query, define the post like so. (data should be a dict)
+   
+    ```
+    emx2.insert(
+      database='mydatabase',
+      query=graphql.insert(table='mytable'),
+      variables={'records': [record-to-import]}
+    )
+    ```
+    """
+    return graphql._operation(operation='insert', table=table)
+
+  def update(table:str=None):
+    """update
+    Query to update record(s) into the database.
+    
+    @param table name of the table to import data into
+    
+    @examples
+    When sending the query, define the post like so. (data should be a dict)
+    
+    ```
+    emx2.update(
+      database='mydatabase',
+      query=graphql.update(table='mytable'),
+      variables={'records': [record-to-import]}
+    )
+    ```
+    """
+    return graphql._operation(operation='update', table=table)

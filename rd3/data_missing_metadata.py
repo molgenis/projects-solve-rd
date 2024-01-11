c21f89210f647aac15bfb1f595abaece7b1dfc28
dcruvolo <davidruvolo51@gmail.com>
2024-01-11 11:17:22 +0100

    feat: added new attributes to dataset

--- rd3/data_missing_metadata.py
+++ rd3/data_missing_metadata.py
@@ -2,9 +2,9 @@
 # FILE: data_missing_metadata.py
 # AUTHOR: David Ruvolo
 # CREATED: 2023-03-22
-# MODIFIED: 2023-03-22
+# MODIFIED: 2024-01-11
 # PURPOSE: find missing metadata and import
-# STATUS: in.progress
+# STATUS: stable
 # PACKAGES: **see below**
 # COMMENTS: NA
 #///////////////////////////////////////////////////////////////////////////////
@@ -12,8 +12,14 @@
 from rd3.api.molgenis2 import Molgenis
 from dotenv import load_dotenv
 from datatable import dt, f, fread
+from datetime import datetime
+from pytz import timezone
 from os import environ
 load_dotenv()
+
+def current_date():
+  """Get the today's date as yyyy-mm-dd"""
+  return datetime.now(tz=timezone('Europe/Amsterdam')).strftime('%Y-%m-%d')
 
 rd3 = Molgenis(environ['MOLGENIS_PROD_HOST'])
 rd3.login(environ['MOLGENIS_PROD_USR'], environ['MOLGENIS_PROD_PWD'])
@@ -24,17 +30,17 @@ rd3.login(environ['MOLGENIS_PROD_USR'], environ['MOLGENIS_PROD_PWD'])
 # Retrieve metadata
 
 # get portal data
-allDT = dt.Frame(
+all_dt = dt.Frame(
   rd3.get(
-    entity='rd3_portal_release_patches',
+    'rd3_portal_release_patches',
+    q='processed==false',
     batch_size=1000
   )
 )
-
-del allDT['_href']
+del all_dt['_href']
 
 # get subject info
-subjectsDT = dt.Frame(
+subjects_dt = dt.Frame(
   rd3.get(
     entity='solverd_subjects',
     attributes='subjectID',
@@ -43,7 +49,7 @@ subjectsDT = dt.Frame(
 )['subjectID']
 
 # get samples
-samplesDT = dt.Frame(
+samples_dt = dt.Frame(
   rd3.get(
     entity='solverd_samples',
     attributes='sampleID',
@@ -52,7 +58,7 @@ samplesDT = dt.Frame(
 )['sampleID']
 
 # get experiments
-experimentsDT = dt.Frame(
+experiments_dt = dt.Frame(
   rd3.get(
     entity = 'solverd_labinfo',
     attributes='experimentID',
@@ -61,44 +67,55 @@ experimentsDT = dt.Frame(
 )['experimentID']
 
 # isolate IDs
-subjectIDs = subjectsDT['subjectID'].to_list()[0]
-sampleIDs = samplesDT['sampleID'].to_list()[0]
-experimentIDs = experimentsDT['experimentID'].to_list()[0]
+subj_ids = subjects_dt['subjectID'].to_list()[0]
+samp_ids = samples_dt['sampleID'].to_list()[0]
+expr_ids = experiments_dt['experimentID'].to_list()[0]
 
 #///////////////////////////////////////////////////////////////////////////////
 
 # ~ 1 ~
 # Identify missing metadata
 
-allDT['isNewSubject'] = dt.Frame([
-  value not in subjectIDs
-  for value in allDT['subject_id'].to_list()[0]
+all_dt['is_new_subject'] = dt.Frame([
+  value not in subj_ids
+  for value in all_dt['subject_id'].to_list()[0]
 ])
 
-allDT['isNewSample'] = dt.Frame([
-  (value not in sampleIDs) and (f'VS{value}' not in sampleIDs)
-  for value in allDT['samples_id'].to_list()[0]
+all_dt['is_new_sample'] = dt.Frame([
+  (value not in samp_ids) and (f'VS{value}' not in samp_ids)
+  for value in all_dt['samples_id'].to_list()[0]
 ])
 
-allDT['isNewExperiment'] = dt.Frame([
-  value not in experimentIDs
-  for value in allDT['samples_id'].to_list()[0]
+all_dt['is_new_experiment'] = dt.Frame([
+  value not in expr_ids
+  for value in all_dt['samples_id'].to_list()[0]
 ])
 
+all_dt['date_added'] = '2023-03-21'
+all_dt['date_last_updated'] = current_date()
+all_dt['processed'] = True
 
-allDT[:, dt.count(), dt.by(f.isNewSubject)]
-allDT[:, dt.count(), dt.by(f.isNewSample)]
-allDT[:, dt.count(), dt.by(f.isNewExperiment)]
-allDT[:, dt.count(), dt.by(f.isNewSubject,f.isNewSample)]
-allDT[:, dt.count(), dt.by(f.isNewSubject,f.isNewSample,f.isNewExperiment)]
+# view records
+all_dt[:, dt.count(), dt.by(f.is_new_subject)]
+all_dt[:, dt.count(), dt.by(f.is_new_sample)]
+all_dt[:, dt.count(), dt.by(f.is_new_experiment)]
+all_dt[:, dt.count(), dt.by(f.is_new_subject,f.is_new_sample)]
+all_dt[:, dt.count(), dt.by(f.is_new_subject,f.is_new_sample,f.is_new_experiment)]
 
-allDT[(f.isNewSubject) | (f.isNewSample), :]
-allDT[f.isNewSample, :]
-allDT[f.isNewExperiment,:]
+all_dt[(f.is_new_subject) | (f.is_new_sample), :]
+all_dt[f.is_new_sample,:]
+all_dt[f.is_new_experiment, :]
 
-newDT = allDT[(f.isNewSubject) | (f.isNewSample) | (f.isNewExperiment), :]
 
-newDT = fread('~/Desktop/rd3_portal_release_new.csv')
+#///////////////////////////////////////////////////////////////////////////////
+
+# ~ 2 ~
+# Import
+
+rd3.importDatatableAsCsv('rd3_portal_release_patches',all_dt)
+
+
+# newDT = all_dt[(f.isNewSubject) | (f.isNewSample) | (f.isNewExperiment), :]
+# newDT = fread('~/Desktop/rd3_portal_release_new.csv')
 # rd3.delete('rd3_portal_release_new')
-
-rd3.importDatatableAsCsv(pkg_entity='rd3_portal_release_new', data=newDT)
+# rd3.importDatatableAsCsv(pkg_entity='rd3_portal_release_new', data=newDT)

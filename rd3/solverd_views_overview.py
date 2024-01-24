813f27d5d1422ef1d3a6a3161d4f94783b092cf9
dcruvolo <davidruvolo51@gmail.com>
2024-01-24 10:31:31 +0100

    integrated py library; revised script

--- rd3/solverd_views_overview.py
+++ rd3/solverd_views_overview.py
@@ -1,214 +1,183 @@
-#'////////////////////////////////////////////////////////////////////////////
-#' FILE: rd3_views_overview.py
-#' AUTHOR: David Ruvolo
-#' CREATED: 2022-05-16
-#' MODIFIED: 2023-02-09
-#' PURPOSE: generate dataset for rd3_overview
-#' STATUS: stable
-#' PACKAGES: **see below**
-#' COMMENTS: NA
-#'////////////////////////////////////////////////////////////////////////////
+"""Solve-RD Overview Mapping
+FILE: rd3_views_overview.py
+AUTHOR: David Ruvolo
+CREATED: 2022-05-16
+MODIFIED: 2024-01-24
+PURPOSE: generate dataset for rd3_overview
+STATUS: stable
+PACKAGES: **see below**
+COMMENTS: NA
+"""
 
-from dotenv import load_dotenv
-from os import environ
-import sys
-load_dotenv()
-sys.path.append(environ['SYS_PATH'])
-
-from rd3.api.molgenis2 import Molgenis
-from rd3.utils.utils import statusMsg, flattenDataset
-from datatable import dt, f, as_type
 import re
+from os import environ
+from dotenv import load_dotenv
+from rd3tools.molgenis import Molgenis
+from rd3tools.utils import print2, flatten_data
+from rd3tools.datatable import unique_values_by_id
+from datatable import dt, f, as_type
+load_dotenv()
 
-def uniqueValuesById(data, groupby, column, dropDuplicates=True, keyGroupBy=True):
-  """Unique Values By Id
-  For a datatable object, collapse all unique values by ID into a comma
-  separated string.
+print2('Connecting to RD3....')
+rd3 = Molgenis(url=environ['MOLGENIS_PROD_HOST'])
+rd3.login(environ['MOLGENIS_PROD_USR'], environ['MOLGENIS_PROD_PWD'])
 
-  @param data datatable object
-  @param groupby name of the column that will serve as the grouping variable
-  @param column name of the column that contains the values to collapse
-  @param dropDuplicates If True, all duplicate rows will be removed
-  @param keyGroupBy If True, returned object will be keyed using the value named in groupby
-  
-  @return datatable object
-  """
-  df = data.to_pandas()
-  df[column] = df.dropna(subset=[column]) \
-    .groupby(groupby)[column] \
-    .transform(lambda val: ','.join(set(map(str, val))))
-  if dropDuplicates:
-    df = df[[groupby, column]].drop_duplicates()
-  output = dt.Frame(df)
-  if keyGroupBy:
-    output.key = groupby
-  return output
 
-#///////////////////////////////////////////////////////////////////////////////
+def get_table_attribs(pkg_entity: str = None, attributes: str = None, nested_columns: str = None):
+    """Retrieve a subset data from a specific table in a database
+    :param pkg_entity: the name of the table in emx format (pkg_entity)
+    :type pkg_entity: str
+
+    :param attributes: one or more columns to retrieve (comma-separated string)
+    :type attributes: str
+
+    :param nested_columns: one or more nested columns to extract when data is
+        flattend (see function `flatten_data`)
+    :type nested_columns: str
+
+    :return: dataset
+    :rtype: datatable frame
+    """
+    data_raw = rd3.get(pkg_entity, attributes=attributes, batch_size=10000)
+    data_flat = flatten_data(data_raw, nested_columns)
+    return dt.Frame(data_flat)
+
+
+# ///////////////////////////////////////////////////////////////////////////////
 
 # ~ 0 ~
 # Fetch RD3 Data
+print2('Pulling metadata....')
 
-if environ['JOB_ENV'] == 'PROD':
-  statusMsg('Connecting to RD3-PROD....')
-  rd3=Molgenis(url=environ['MOLGENIS_PROD_HOST'])
-  rd3.login(environ['MOLGENIS_PROD_USR'],environ['MOLGENIS_PROD_PWD'])
-else:
-  statusMsg('Connecting to RD3-ACC....')
-  rd3=Molgenis(environ['MOLGENIS_ACC_HOST'])
-  rd3.login(environ['MOLGENIS_ACC_USR'], environ['MOLGENIS_ACC_PWD'])
+subjects_dt = get_table_attribs(
+    pkg_entity='solverd_subjects',
+    attributes=','.join([
+        'subjectID', 'sex1', 'fid', 'mid', 'pid', 'clinical_status', 'solved',
+        'disease', 'phenotype', 'hasNotPhenotype', 'organisation', 'ERN', 'partOfRelease'
+    ]),
+    nested_columns='subjectID|id|value'
+)
 
+samples_dt = get_table_attribs(
+    pkg_entity='solverd_samples',
+    attributes='sampleID,belongsToSubject',
+    nested_columns='subjectID'
+)
 
-# ~ 0a ~
-# Pull All Subject Metadata
-statusMsg('Pulling subject metadata....')
+experiments_dt = get_table_attribs(
+    pkg_entity='solverd_labinfo',
+    attributes='experimentID,sampleID',
+    nested_columns='sampleID'
+)
 
-columns=','.join([
-  'subjectID', 'sex1', 'fid', 'mid', 'pid', 'clinical_status', 'solved',
-  'disease', 'phenotype', 'hasNotPhenotype', 'organisation', 'ERN', 'partOfRelease'
+# ///////////////////////////////////////////////////////////////////////////////
+
+# ~ 1 ~
+# Summarise data
+print2('Summarising sample metadata by subject....')
+
+subjects_dt['hasOnlyNovelOmics'] = dt.Frame([
+    not bool(re.search('freeze', d))
+    for d in subjects_dt['partOfRelease'].to_list()[0]
 ])
 
-rawsubjects = rd3.get('solverd_subjects',attributes=columns,batch_size=1000)
-subjectsflattened = flattenDataset(rawsubjects, columnPatterns='subjectID|id|value')
-subjects=dt.Frame(subjectsflattened)
-
-#///////////////////////////////////////
-
-# ~ 1b ~
-# fetch sample metadata
-statusMsg('Pulling sample metadata....')
-rawsamples = rd3.get(
-  'solverd_samples',
-  batch_size=10000,
-  attributes='sampleID,belongsToSubject',
-  q = 'retracted==N'
+# get all samples by subject
+samples_by_subj = unique_values_by_id(
+    data=samples_dt[:, (f.belongsToSubject, f.sampleID)],
+    group_by='belongsToSubject',
+    column='sampleID'
 )
 
-samplesflattened = flattenDataset(rawsamples,columnPatterns='subjectID')
-samples = dt.Frame(samplesflattened)
+# get number of samples by subject
+sample_counts_by_subj = samples_dt[:, dt.count(), dt.by(f.belongsToSubject)]
+sample_counts_by_subj.key = 'belongsToSubject'
 
-
-# summarize sampleIDs by subjectID
-statusMsg('Summarising sample metadata by subject....')
-samplesBySubject = uniqueValuesById(
-  data = samples[:, (f.belongsToSubject, f.sampleID)],
-  groupby='belongsToSubject',
-  column ='sampleID'
-)
-
-# summarize number of samples per subject
-statusMsg('Preparing sample data for join....')
-
-sampleCountsBySubject = samples[:, dt.count(), dt.by(f.belongsToSubject)]
-sampleCountsBySubject.key = 'belongsToSubject'
-
-samplesBySubject = samplesBySubject[:, :, dt.join(sampleCountsBySubject)]
-samplesBySubject.names = {
-  'belongsToSubject': 'subjectID',
-  'sampleID': 'samples',
-  'count': 'numberOfSamples'
+# join samples and counts
+samples_by_subj = samples_by_subj[:, :, dt.join(sample_counts_by_subj)]
+samples_by_subj.names = {
+    'belongsToSubject': 'subjectID',
+    'sampleID': 'samples',
+    'count': 'numberOfSamples'
 }
 
 # join data
-statusMsg('Joining sample metadata with subjects....')
-subjects.key = 'subjectID'
-samplesBySubject.key = 'subjectID'
-subjects = subjects[:, :, dt.join(samplesBySubject)]
+subjects_dt.key = 'subjectID'
+samples_by_subj.key = 'subjectID'
+subjects_dt = subjects_dt[:, :, dt.join(samples_by_subj)]
 
-#///////////////////////////////////////
-
-# ~ 0c ~    
-# fetch experiment metadata
-statusMsg('Pulling experiment metadata....')
-rawexperiments = rd3.get(
-  'solverd_labinfo',
-  batch_size=10000,
-  attributes = 'experimentID,sampleID',
-  q='retracted==N'
-)
-
-experimentsflattened = flattenDataset(rawexperiments,columnPatterns='sampleID')
-experiments=dt.Frame(rawexperiments)
+# ///////////////////////////////////////
 
 # join subject ID
-statusMsg('Joining experiment data with samples....')
-experiments.key = 'sampleID'
-samples.key = 'sampleID'
-experiments = experiments[:, :, dt.join(samples)]
+print2('Summarising experiment metadata by samples....')
+samples_dt.key = 'sampleID'
+experiments_dt = experiments_dt[:, :, dt.join(samples_dt)]
 
 # summarize data
-statusMsg('Summarizing experiment data by subject and generating counts....')
-experimentCountsBySubject = experiments[:, dt.count(f.experimentID), dt.by(f.belongsToSubject)]
-experimentCountsBySubject.names = {'experimentID': 'numberOfExperiments'}
+expr_count_by_subj = experiments_dt[
+    :, dt.count(f.experimentID), dt.by(f.belongsToSubject)]
+
+expr_count_by_subj.names = {'experimentID': 'numberOfExperiments'}
 
 # summarize samples by subject
-experimentsBySubject = uniqueValuesById(
-  data = experiments[:, (f.belongsToSubject, f.experimentID)],
-  groupby = 'belongsToSubject',
-  column = 'experimentID'
+expr_by_subject = unique_values_by_id(
+    data=experiments_dt[:, (f.belongsToSubject, f.experimentID)],
+    group_by='belongsToSubject',
+    column='experimentID'
 )
 
 # join counts with experiment summary
-experimentsBySubject.key = 'belongsToSubject'
-experimentCountsBySubject.key = 'belongsToSubject'
-experimentsBySubject = experimentsBySubject[:, :, dt.join(experimentCountsBySubject)]
+expr_by_subject.key = 'belongsToSubject'
+expr_count_by_subj.key = 'belongsToSubject'
+expr_by_subject = expr_by_subject[:, :, dt.join(expr_count_by_subj)]
 
 # join with main dataset
-statusMsg('Joining experiment data with subjects....')
-experimentsBySubject.names = {'experimentID': 'experiments', 'belongsToSubject': 'subjectID'}
-experimentsBySubject.key = 'subjectID'
-subjects.key = 'subjectID'
+expr_by_subject.names = {
+    'experimentID': 'experiments',
+    'belongsToSubject': 'subjectID'
+}
 
-subjects = subjects[:, :, dt.join(experimentsBySubject)]
+expr_by_subject.key = 'subjectID'
+subjects_dt.key = 'subjectID'
+subjects_dt = subjects_dt[:, :, dt.join(expr_by_subject)]
 
-#///////////////////////////////////////
+# ///////////////////////////////////////
 
-# ~ 0d ~
 # Add File query
 # Since the files table is quite large and it takes a while to pull the data,
 # it is better to make a query URL that redirects to files tables.
+# print2('Getting file metadata.....')
+# files_dt = get_table_attribs(
+#     pkg_entity='solverd_files',
+#     attributes='subjectID',
+#     nested_columns='subjectID'
+# )
 
-# get file metadata -- subjects only
-statusMsg('Getting file metadata.....')
-rawfiles = rd3.get(
-  entity='solverd_files',
-  attributes='subjectID',
-  batch_size=1000
-)
+# file_subject_ids = dt.unique(
+#     files_dt[f.subjectID != None, 'subjectID']).to_list()[0]
 
-files = dt.Frame(flattenDataset(rawfiles,columnPatterns="subjectID"))
-files = dt.unique(files[f.subjectID!=None,'subjectID'])
-fileSubjectIDs =files['subjectID'].to_list()[0]
+# subjects_dt['files'] = dt.Frame([
+#     f'?entity=solverd_files&hideselect=true&mod=data&filter=subjectID=={value}'
+#     if value in file_subject_ids else None
+#     for value in subjects_dt['subjectID'].to_list()[0]
+# ])
 
-statusMsg('Create redirect for files table....')
-subjects['files'] = dt.Frame([
-  f'?entity=solverd_files&hideselect=true&mod=data&filter=subjectID=={id}'
-  if id in fileSubjectIDs else None
-  for id in subjects['subjectID'].to_list()[0]
-])
+# ///////////////////////////////////////////////////////////////////////////////
 
-#///////////////////////////////////////////////////////////////////////////////
-
-# ~ 1 ~
+# ~ 3 ~
 # Import Data
-statusMsg('Detecting novel omics only samples....')
 
-# compute 'hasOnlyNovelOmics' stauts
-subjects['hasOnlyNovelOmics'] = dt.Frame([
-  not bool(re.search('freeze', d))
-  for d in subjects['partOfRelease'].to_list()[0]
-])
+print2('Renaming columns and importing....')
+subjects_dt['hasOnlyNovelOmics'] = subjects_dt[
+    :, as_type(f.hasOnlyNovelOmics, dt.Type.str32)]
 
-# prep columns
-statusMsg('Renaming columns....')
-subjects[
-  :, dt.update(
-    hasOnlyNovelOmics = as_type(f.hasOnlyNovelOmics, str),
-    clinical_status = as_type(f.clinical_status, str),
-    numberOfSamples = as_type(f.numberOfSamples, str),
-    numberOfExperiments = as_type(f.numberOfExperiments, str),
-)]
+subjects_dt['clinical_status'] = subjects_dt[
+    :, as_type(f.clinical_status, dt.Type.str32)]
 
-# import
-statusMsg('Importing overview dataset into RD3.....')
-rd3.importDatatableAsCsv('solverd_overview', data = subjects)
+subjects_dt['numberOfSamples'] = subjects_dt[
+    :, as_type(f.numberOfSamples, dt.Type.str32)]
+
+subjects_dt['numberOfExperiments'] = subjects_dt[
+    :, as_type(f.numberOfExperiments, dt.Type.str32)]
+
+rd3.import_dt('solverd_overview', subjects_dt)
+rd3.logout()

f6c8b5e0dcd758d82f8df8689429dc6020e6a2b0
dcruvolo <davidruvolo51@gmail.com>
2024-01-30 14:30:21 +0100

    renamed python library dir

---
+++ rd3_py/dev/dev.py
@@ -0,0 +1,61 @@
+# ///////////////////////////////////////////////////////////////////////////////
+# FILE: dev.py
+# AUTHOR: David Ruvolo
+# CREATED: 2024-01-17
+# MODIFIED: 2024-01-17
+# PURPOSE: debug functions in the lib
+# STATUS: in.progress
+# PACKAGES: **see below**
+# COMMENTS: NA
+# ///////////////////////////////////////////////////////////////////////////////
+
+import random
+from datatable import dt
+
+from .src.rd3tools.utils import (
+    as_key_pairs,
+    flatten_data,
+    print2,
+    recode_value
+)
+
+from rd3tools.src.rd3tools.molgenis import Molgenis
+
+# print message
+print2('Test', 'tests')
+
+
+# define data
+data = [
+    {'group': 'A', 'label': 'Group A', 'result': {'id': 'A-Q123'}},
+    {'group': 'B', 'label': 'Group B', 'result': {'id': 'B-Q123'}},
+    {'group': 'C', 'label': 'Group C', 'result': {'id': 'C-Q123'}},
+    {'group': 'D', 'label': 'Group D', 'result': {'id': 'D-Q123'}},
+]
+
+# as pairs
+pairs = as_key_pairs(data, 'group', 'label')
+
+# recode values
+recode_value(mappings=pairs, value='A', label='Group')
+recode_value(mappings=pairs, value='B', label='Group')
+recode_value(mappings=pairs, value='C', label='Group')
+recode_value(mappings=pairs, value='D', label='Group')
+
+# flatten data
+data_flat = flatten_data(data=data, nested_keys='id')
+
+
+# connect to database
+db = Molgenis('')
+db.login('', '')
+
+d = dt.Frame([
+    {
+        'id': str(random.random()).split('.')[1],
+        'label': 'Test',
+        'value': random.random()
+    }
+])
+
+db.import_dt('', d)
--- rd3_py/src/rd3tools/molgenis.py
+++ rd3_py/src/rd3tools/molgenis.py
@@ -5,8 +5,9 @@ import tempfile
 import csv
 import numpy as np
 import molgenis.client as molgenis
+from datatable import dt
 
-from .utils import print2
+from .utils import print2, flatten_data
 
 
 class Molgenis(molgenis.Session):
@@ -66,3 +67,33 @@ class Molgenis(molgenis.Session):
 
                 file.close()
             return response
+
+
+def get_table_attribs(
+        client,
+        pkg_entity: str = None,
+        columns: str = None,
+        query: str = None,
+        nested_columns: str = None
+):
+    """Retrieve a flattened datatable object from a table in a database by specific
+    columns or filter.
+
+    :param pkg_entity: the name of the table in emx format (pkg_entity)
+    :type pkg_entity: str
+
+    :param nested_columns: one or more nested columns to extract when data is
+        flattend (see function `flatten_data`)
+    :type nested_columns: str
+
+    :param *kwargs: additional params to pass on to `get`
+
+    :return: dataset
+    :rtype: datatable frame
+    """
+    data_raw = client.get(entity=pkg_entity,
+                          attributes=columns,
+                          q=query,
+                          batch_size=10000)
+    data_flat = flatten_data(data_raw, nested_columns)
+    return dt.Frame(data_flat)

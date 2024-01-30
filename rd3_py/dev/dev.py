ff7b1070c3bd3208820b2ef38686c17e87f8821c
dcruvolo <davidruvolo51@gmail.com>
2024-01-30 15:10:45 +0100

    added new function to package

--- rd3_py/dev/dev.py
+++ rd3_py/dev/dev.py
@@ -1,25 +1,30 @@
-# ///////////////////////////////////////////////////////////////////////////////
-# FILE: dev.py
-# AUTHOR: David Ruvolo
-# CREATED: 2024-01-17
-# MODIFIED: 2024-01-17
-# PURPOSE: debug functions in the lib
-# STATUS: in.progress
-# PACKAGES: **see below**
-# COMMENTS: NA
-# ///////////////////////////////////////////////////////////////////////////////
+"""Dev script
+FILE: dev.py
+AUTHOR: David Ruvolo
+CREATED: 2024-01-17
+MODIFIED: 2024-01-17
+PURPOSE: debug functions in the lib
+STATUS: in.progress
+PACKAGES: **see below**
+COMMENTS: NA
+"""
 
 import random
+from os import environ
 from datatable import dt
-
-from .src.rd3tools.utils import (
+from dotenv import load_dotenv
+from rd3_py.src.rd3tools.molgenis import Molgenis, get_table_attribs
+from rd3_py.src.rd3tools.utils import (
     as_key_pairs,
     flatten_data,
     print2,
     recode_value
 )
 
-from rd3tools.src.rd3tools.molgenis import Molgenis
+# connect to database
+load_dotenv()
+db = Molgenis(environ['MOLGENIS_PROD_HOST'])
+db.login(environ['MOLGENIS_PROD_USR'], environ['MOLGENIS_PROD_PWD'])
 
 # print message
 print2('Test', 'tests')
@@ -43,12 +48,8 @@ recode_value(mappings=pairs, value='C', label='Group')
 recode_value(mappings=pairs, value='D', label='Group')
 
 # flatten data
-data_flat = flatten_data(data=data, nested_keys='id')
+data_flat = flatten_data(data=data, col_patterns='id')
 
-
-# connect to database
-db = Molgenis('')
-db.login('', '')
 
 d = dt.Frame([
     {
@@ -59,3 +60,8 @@ d = dt.Frame([
 ])
 
 db.import_dt('', d)
+
+data = get_table_attribs(
+    client=db,
+    pkg_entity='solverdportal_experiment_errors'
+)
